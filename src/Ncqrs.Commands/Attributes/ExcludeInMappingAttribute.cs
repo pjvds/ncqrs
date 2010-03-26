31dd97f3944f134546a5de5ef254bed64cf7bb2d
pj@born2code.net <pj@born2code.net>
2010-03-26 12:38:45 +0000

    AddAdded attributes to add meta information to commands.

---
+++ src/Ncqrs.Commands/Attributes/ExcludeInMappingAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ncqrs.Commands.Attributes
+{
+    /// <summary>
+    /// Defines that a property should be excluded in the auto mapping process.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class ExcludeInMappingAttribute : Attribute
+    {
+    }
+}
