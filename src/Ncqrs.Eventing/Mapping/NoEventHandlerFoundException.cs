139dd8a1b7cff0b883454493554429028e8073ca
pj@born2code.net <pj@born2code.net>
2010-04-02 09:01:37 +0000

    Added documentation to the NoEventHandlerFoundException class.

--- src/Ncqrs.Eventing/Mapping/NoEventHandlerFoundException.cs
+++ src/Ncqrs.Eventing/Mapping/NoEventHandlerFoundException.cs
@@ -2,26 +2,46 @@
 
 namespace Ncqrs.Eventing.Mapping
 {
+    /// <summary>
+    /// Occurs when no handler was found for for a <see cref="IEvent"/>.
+    /// </summary>
     public class NoEventHandlerFoundException : Exception
     {
+        /// <summary>
+        /// Gets the event.
+        /// </summary>
         public IEvent Event
         {
             get;
             private set;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NoEventHandlerFoundException"/> class.
+        /// </summary>
+        /// <param name="evnt">The event.</param>
         public NoEventHandlerFoundException(IEvent evnt)
-            : this(String.Format("No handler found for event {0}.", evnt), evnt)
-        {
-
-        }
-
-        public NoEventHandlerFoundException(string message, IEvent evnt)
-            : this(message, evnt, null)
+            : this(evnt, String.Format("No handler found for event {0}.", evnt))
         {
         }
 
-        public NoEventHandlerFoundException(string message, IEvent evnt, Exception innerException)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NoEventHandlerFoundException"/> class.
+        /// </summary>
+        /// <param name="evnt">The evnt.</param>
+        /// <param name="message">The message.</param>
+        public NoEventHandlerFoundException(IEvent evnt, string message)
+            : this(evnt, message, null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NoEventHandlerFoundException"/> class.
+        /// </summary>
+        /// <param name="evnt">The evnt.</param>
+        /// <param name="message">The message.</param>
+        /// <param name="innerException">The inner exception.</param>
+        public NoEventHandlerFoundException(IEvent evnt, string message, Exception innerException)
             : base(message, innerException)
         {
             Event = evnt;
