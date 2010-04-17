ca8a0be69fe31f9f626f94b10c964d53b4463180
pj@born2code.net <pj@born2code.net>
2010-04-17 11:20:59 +0200

    Added extra documentation to the NcqrsEnvironment class. Also added exception behavior when a instance is requested, but the environment was not configured.

--- src/Framework/Ncqrs/Config/NcqrsEnvironment.cs
+++ src/Framework/Ncqrs/Config/NcqrsEnvironment.cs
@@ -14,15 +14,23 @@ namespace Ncqrs.Config
     public class NcqrsEnvironment
     {
         private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        /// <summary>
+        /// Hold the environment configuration. This is initialized by the <see cref="Configure"/> method.
+        /// </summary>
         private static IEnvironmentConfiguration _instance;
 
         /// <summary>
         /// Gets or create the requested instance specified by yhe parameter <i>T</i>.
         /// </summary>
         /// <typeparam name="T">The type of the instance that is requested.</typeparam>
+        /// <exception cref="EnvironmentNotConfiguredException">Occurs when the environment is not configured. Call <see cref="Configure"/> to configure
+        ///   the environment before requesting an instance.</exception>
         /// <returns>The instance of the requested type specified by <i>T</i>.</returns>
         public static T Get<T>() where T : class
         {
+            if(_instance == null) throw new EnvironmentNotConfiguredException("The Ncqrs environment is not configured. Use the Configure method to configure it first.");
+
             Log.DebugFormat("Requested instance {0} from the environment.", typeof(T).FullName);
 
             return _instance.Get<T>();
