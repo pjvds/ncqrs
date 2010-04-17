8f4096ea8ca9539e156d5ec4e065a10ae4f76bbf
pj@born2code.net <pj@born2code.net>
2010-04-17 11:14:23 +0200

    Fixed missing reference to log4net in the Ncqrs project.

--- src/Framework/Ncqrs/Config/NcqrsEnvironment.cs
+++ src/Framework/Ncqrs/Config/NcqrsEnvironment.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Configuration;
 using System.Diagnostics.Contracts;
 using log4net;
 
