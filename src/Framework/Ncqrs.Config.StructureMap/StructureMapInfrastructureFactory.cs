9c8008ba043a94d7f6e5834c9d7328eb68c79b43
pj@born2code.net <pj@born2code.net>
2010-04-17 10:55:28 +0200

    Added generics contrains to the StructureMapConfiguration class, since the IEnvironmentConfiguration requires it.

--- src/Framework/Ncqrs.Config.StructureMap/StructureMapInfrastructureFactory.cs
+++ src/Framework/Ncqrs.Config.StructureMap/StructureMapInfrastructureFactory.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using StructureMap;
 
 namespace Ncqrs.Config.StructureMap
 {
+    /// <summary>
+    /// A environment configuration based on structure map. All requested instances
+    /// will be get from the structure map <see cref="ObjectFactory"/>.
+    /// </summary>
     public class StructureMapConfiguration : IEnvironmentConfiguration
     {
         public StructureMapConfiguration(Action<IInitializationExpression> initialization = null, Action<ConfigurationExpression> configuration = null)
@@ -20,13 +21,13 @@ namespace Ncqrs.Config.StructureMap
             }
         }
 
-        public T Get<T>()
+        public T Get<T>() where T : class
         {
             return ObjectFactory.GetInstance<T>();
         }
 
 
-        public bool TryGet<T>(out T result)
+        public bool TryGet<T>(out T result) where T : class
         {
             result = default(T);
             var foundInstance = ObjectFactory.TryGetInstance(typeof(T));
