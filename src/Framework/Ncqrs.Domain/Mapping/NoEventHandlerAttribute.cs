01cba0467b2315a7ffcb737eebbb514237a26810
pj@born2code.net <pj@born2code.net>
2010-04-21 20:59:02 +0200

    Added documentation to the NoEventHandlerAttribute class.

--- src/Framework/Ncqrs.Domain/Mapping/NoEventHandlerAttribute.cs
+++ src/Framework/Ncqrs.Domain/Mapping/NoEventHandlerAttribute.cs
@@ -1,10 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Ncqrs.Domain.Mapping
 {
+    /// <summary>
+    /// Specifies that the method is not an event handler. Use this attribute to exclude the
+    /// method from the <see cref="ConventionBasedInternalEventHandlerMappingStrategy"/>.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
     public sealed class NoEventHandlerAttribute : Attribute
     {
