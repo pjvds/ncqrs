8e975aa7f5dc9db7bb3703925755c6c5831da70e
pj@born2code.net <pj@born2code.net>
2010-04-22 12:33:43 +0200

    Removed old projects, since they moved into the Ncqrs project file.

--- Framework/src/Ncqrs.Eventing/EventAppliedEventHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Ncqrs.Domain
-{
-    public delegate void EventAppliedEventHandler(EventSource sender, EventAppliedEventArgs e);
-}
