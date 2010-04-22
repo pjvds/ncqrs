5279893872d9a2e37a41c443fd50b1d2e9e76889
pj@born2code.net <pj@born2code.net>
2010-04-22 21:55:03 +0200

    Introduced defaults support in NcqrsEnvironment class.

--- Framework/src/Ncqrs/IUniqueIdentifierGenerator.cs
+++ Framework/src/Ncqrs/IUniqueIdentifierGenerator.cs
@@ -5,5 +5,6 @@ namespace Ncqrs
     public interface IUniqueIdentifierGenerator
     {
         Guid GenerateNewId();
+ 
     }
 }
--- Framework/src/Ncqrs/NcqrsEnvironment.cs
+++ Framework/src/Ncqrs/NcqrsEnvironment.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Diagnostics.Contracts;
 using log4net;
@@ -14,7 +15,22 @@ namespace Ncqrs
     /// </summary>
     public static class NcqrsEnvironment
     {
+        static NcqrsEnvironment()
+        {
+            // Initialize defaults.
+            SetDefault<IClock>(new DateTimeBasedClock());
+            SetDefault<IUniqueIdentifierGenerator>(new BasicGuidGenerator());
+        }
+
         private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        /// <summary>
+        /// Holds the defaults for requested types that are not configured.
+        /// </summary>
+        /// <remarks>
+        /// Use the <see cref="SetDefault{T}"/> method to set a default.
+        /// </remarks>
+        private static readonly Dictionary<Type, Object> _defaults = new Dictionary<Type, object>(0);
 
         /// <summary>
         /// Hold the environment configuration. This is initialized by the <see cref="Configure"/> method.
@@ -25,23 +41,43 @@ namespace Ncqrs
         /// Gets or create the requested instance specified by yhe parameter <i>T</i>.
         /// </summary>
         /// <typeparam name="T">The type of the instance that is requested.</typeparam>
-        /// <exception cref="EnvironmentNotConfiguredException">Occurs when the environment is not configured. Call <see cref="Configure"/> to configure
-        ///   the environment before requesting an instance.</exception>
         /// <returns>The instance of the requested type specified by <i>T</i>.</returns>
         public static T Get<T>() where T : class
         {
-            if(_instance == null) throw new EnvironmentNotConfiguredException("The Ncqrs environment is not configured. Use the Configure method to configure it first.");
-
             Log.DebugFormat("Requesting instance {0} from the environment.", typeof(T).FullName);
 
             T result;
 
-            if (!_instance.TryGet(out result))
+            if (_instance == null || !_instance.TryGet(out result))
             {
-                throw new InstanceNotFoundInEnvironmentConfigurationException(typeof (T));
+                object defaultResult;
+
+                if (!_defaults.TryGetValue(typeof(T), out defaultResult))
+                {
+                    throw new InstanceNotFoundInEnvironmentConfigurationException(typeof(T));
+                }
+
+                result = (T) defaultResult;
             }
 
             return result;
+        }
+
+        /// <summary>
+        /// Sets the default for an type. This default instance is returned when
+        /// the configured <see cref="IEnvironmentConfiguration"/> did not
+        /// returned an instance for this type.
+        /// </summary>
+        /// <remarks>When the type already contains a default, it is overridden.
+        /// </remarks>
+        /// <typeparam name="T">The type of the instance to set a default.
+        /// </typeparam>
+        /// <param name="instance">The instance to set as default.</param>
+        public static void SetDefault<T>(T instance) where T : class
+        {
+            Contract.Requires<ArgumentNullException>(instance != null, "The instance cannot be null.");
+
+            _defaults[typeof (T)] = instance;
         }
 
         /// <summary>
