f762ade85d055d9c52589d8f7fcab8e34975b3e3
pj@born2code.net <pj@born2code.net>
2010-04-22 11:38:37 +0200

    Added Ncqrs root namespace documentation.

---
+++ Framework/src/Ncqrs/NamespaceDoc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Ncqrs.Domain;
+using Ncqrs.Domain.Storage;
+
+namespace Ncqrs
+{
+    /// <summary>
+    /// This namespace contains the components of the Ncqrs framework. The framework is devided in tree main sections. 
+    /// <para>
+    /// <b>Commanding</b> is the section where everything related to commands, command execution and command services can be found.
+    /// </para>
+    /// <para>
+    /// <b>Domain</b> is the section where everything related to the domain can be found. In this section you find things like
+    /// <see cref="AggregateRoot"/> and the <see cref="IDomainRepository"/>.
+    /// </para>
+    /// <para>
+    /// <b>Eventing</b> is the section where everything related to events, denormalization and event services can be found.
+    /// </para>
+    /// <para>For more information about the framework, visit <seealso cref="http://ncqrs.org">ncqrs.org</seealso> or visit the 
+    /// <seealso cref="http://blog.ncqrs.org">blog</seealso>.</para>
+    /// </summary>
+    [CompilerGenerated]
+    class NamespaceDoc
+    {
+        // This class is only used to document the namespace.
+        // Do not add any code to this class!
+    }
+}
