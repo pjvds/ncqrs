bb8c482b8eeb033b6d6f45ee23d03af2040d737e
pj@born2code.net <pj@born2code.net>
2010-04-22 16:28:06 +0200

    Introduced IClock and basic DateTimeBasedClock implementation.

---
+++ Framework/src/Ncqrs/DateTimeBasedClock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ncqrs
+{
+    /// <summary>
+    /// A clock that is based on the <see cref="DateTime"/> class from the .NET framework.
+    /// </summary>
+    public class DateTimeBasedClock : IClock
+    {
+        /// <summary>
+        /// Gets the current UTC date and time from the clock.
+        /// </summary>
+        /// <returns>The current UTC date and time.</returns>
+        public DateTime UtcNow()
+        {
+            return DateTime.UtcNow;
+        }
+    }
+}
---
+++ Framework/src/Ncqrs/IClock.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace Ncqrs
+{
+    /// <summary>
+    /// Represents a clock that can give the current time.
+    /// </summary>
+    [ContractClass(typeof(IClockContracts))]
+    public interface IClock
+    {
+        /// <summary>
+        /// Gets the current UTC date and time from the clock.
+        /// </summary>
+        /// <returns>The current UTC date and time.</returns>
+        DateTime UtcNow();
+    }
+
+    [ContractClassFor(typeof(IClock))]
+    internal sealed class IClockContracts : IClock
+    {
+        public DateTime UtcNow()
+        {
+            Contract.Ensures(Contract.Result<DateTime>().Kind == DateTimeKind.Utc, "The result should be a UTC date and time.");
+            return default(DateTime);
+        }
+    }
+}
