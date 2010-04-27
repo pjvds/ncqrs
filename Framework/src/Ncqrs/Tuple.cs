4299f54c0e3c0a0bc4a641f1eaab42139d1ec69e
pj@born2code.net <pj@born2code.net>
2010-04-27 20:44:22 +0200

    Added Tuple class since the .NET 3.5 BCL does not contain it.

---
+++ Framework/src/Ncqrs/Tuple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace System
+{
+    public class Tuple<T1, T2>
+    {
+        public T1 Type1
+        {
+            get; private set;
+        }
+
+        public T2 Type2
+        {
+            get; private set;
+        }
+
+        public Tuple(T1 type1, T2 type2)
+        {
+            Type1 = type1;
+            Type2 = type2;
+        }
+    }
+}
