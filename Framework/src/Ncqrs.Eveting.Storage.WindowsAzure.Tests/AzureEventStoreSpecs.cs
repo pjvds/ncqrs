eb32c1a1f3e55c8a4381d921d0450b535f4253a1
pj@born2code.net <pj@born2code.net>
2010-04-30 10:39:18 +0200

    Removed try-catch with empty catch blocks in the AzureEventStoreSpecs.

--- Framework/src/Ncqrs.Eveting.Storage.WindowsAzure.Tests/AzureEventStoreSpecs.cs
+++ Framework/src/Ncqrs.Eveting.Storage.WindowsAzure.Tests/AzureEventStoreSpecs.cs
@@ -53,72 +53,51 @@ namespace Ncqrs.Eventing.Storage.WindowsAzure.Tests
         [Test]
         public void All_the_events_from_a_new_event_source_should_add_events_to_event_store()
         {
-            try
-            {
-                var mock = MockRepository.GenerateMock<IEventSource>();
-                mock.Stub(m => mock.Id).Return(Guid.NewGuid()).Repeat.Any();
-                mock.Stub(m => mock.GetUncommittedEvents()).Return(new ISourcedEvent[] { new FooEvent(mock.Id, 1), new BarEvent(mock.Id, 2) });
+            var mock = MockRepository.GenerateMock<IEventSource>();
+            mock.Stub(m => mock.Id).Return(Guid.NewGuid()).Repeat.Any();
+            mock.Stub(m => mock.GetUncommittedEvents()).Return(new ISourcedEvent[] { new FooEvent(mock.Id, 1), new BarEvent(mock.Id, 2) });
 
-                var theEventStore = new AzureEventStore();
-                theEventStore.Save(mock);
+            var theEventStore = new AzureEventStore();
+            theEventStore.Save(mock);
 
-                var storedEvents = theEventStore.GetAllEventsForEventSource(mock.Id);
-                storedEvents.Count().Should().Be(2);
-            }
-            catch (Exception e)
-            {
-                throw;
-            }
+            var storedEvents = theEventStore.GetAllEventsForEventSource(mock.Id);
+            storedEvents.Count().Should().Be(2);
         }
 
         [Test]
         public void All_the_events_from_a_event_source_should_add_events_to_event_store()
         {
-            try
-            {
-                var mock = MockRepository.GenerateStub<IEventSource>();
-                mock.Stub(m => mock.Id).Return(Guid.NewGuid()).Repeat.Any();
-                mock.Stub(m => mock.GetUncommittedEvents()).Return(new ISourcedEvent[] { new FooEvent(mock.Id, 1), new BarEvent(mock.Id, 2) }).Repeat.Once();
+            var mock = MockRepository.GenerateStub<IEventSource>();
+            mock.Stub(m => mock.Id).Return(Guid.NewGuid()).Repeat.Any();
+            mock.Stub(m => mock.GetUncommittedEvents()).Return(new ISourcedEvent[] { new FooEvent(mock.Id, 1), new BarEvent(mock.Id, 2) }).Repeat.Once();
 
-                var theEventStore = new AzureEventStore();
-                theEventStore.Save(mock);
+            var theEventStore = new AzureEventStore();
+            theEventStore.Save(mock);
 
-                mock.Stub(m => mock.Version).Return(2);
-                mock.Stub(m => mock.GetUncommittedEvents()).Return(new ISourcedEvent[] { new FooEvent(mock.Id, 3), new BarEvent(mock.Id, 4) });
-                theEventStore.Save(mock);
+            mock.Stub(m => mock.Version).Return(2);
+            mock.Stub(m => mock.GetUncommittedEvents()).Return(new ISourcedEvent[] { new FooEvent(mock.Id, 3), new BarEvent(mock.Id, 4) });
+            theEventStore.Save(mock);
 
 
-                var storedEvents = theEventStore.GetAllEventsForEventSource(mock.Id);
-                storedEvents.Count().Should().Be(4);
-            }
-            catch (Exception e)
-            {
-                throw;
-            }
+            var storedEvents = theEventStore.GetAllEventsForEventSource(mock.Id);
+            storedEvents.Count().Should().Be(4);
         }
 
         [Test]
         public void Saving_an_old_event_source_should_throw_exception()
         {
-            try
-            {
-                var mock = MockRepository.GenerateStub<IEventSource>();
-                mock.Stub(m => mock.Id).Return(Guid.NewGuid()).Repeat.Any();
-                mock.Stub(m => mock.GetUncommittedEvents()).Return(new ISourcedEvent[] { new FooEvent(mock.Id, 1), new BarEvent(mock.Id, 2) }).Repeat.Once();
+            var mock = MockRepository.GenerateStub<IEventSource>();
+            mock.Stub(m => mock.Id).Return(Guid.NewGuid()).Repeat.Any();
+            mock.Stub(m => mock.GetUncommittedEvents()).Return(new ISourcedEvent[] { new FooEvent(mock.Id, 1), new BarEvent(mock.Id, 2) }).Repeat.Once();
 
-                var theEventStore = new AzureEventStore();
-                theEventStore.Save(mock);
+            var theEventStore = new AzureEventStore();
+            theEventStore.Save(mock);
 
-                mock.Stub(m => mock.Version).Return(1);
-                mock.Stub(m => mock.GetUncommittedEvents()).Return(new ISourcedEvent[] { new FooEvent(mock.Id, 3), new BarEvent(mock.Id, 4) });
-                Action act = ()=>theEventStore.Save(mock);
+            mock.Stub(m => mock.Version).Return(1);
+            mock.Stub(m => mock.GetUncommittedEvents()).Return(new ISourcedEvent[] { new FooEvent(mock.Id, 3), new BarEvent(mock.Id, 4) });
+            Action act = () => theEventStore.Save(mock);
 
-                act.ShouldThrow<ConcurrencyException>();
-            }
-            catch (Exception e)
-            {
-                throw;
-            }
+            act.ShouldThrow<ConcurrencyException>();
         }
     }
 }
