1f873e6418a892818a8281de30ef2a639ba4a702
pj@born2code.net <pj@born2code.net>
2010-04-30 10:04:14 +0200

    Added tests for the AggregateRootMappedWithAttributes class.

--- Framework/src/Ncqrs.Tests/Domain/AggregateRootMappedByConventionTests.cs
+++ Framework/src/Ncqrs.Tests/Domain/AggregateRootMappedByConventionTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using FluentAssertions;
 using Ncqrs.Domain;
@@ -65,6 +66,16 @@ namespace Ncqrs.Tests.Domain
             {
                 OnEventForNoEventHandlerMethodInvokeCount++;
             }
+        }
+
+        [Test]
+        public void Initializing_one_should_set_the_mapping_strategy_to_convention_based()
+        {
+            var aggregateRoot = MockRepository.GenerateMock<AggregateRootMappedByConvention>();
+            var field = aggregateRoot.GetType().BaseType.BaseType.GetField("_mappingStrategy", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.GetField);
+
+            var theStrategy = field.GetValue(aggregateRoot);
+            theStrategy.Should().BeOfType<ConventionBasedDomainEventHandlerMappingStrategy>();
         }
 
         [Test]
---
+++ Framework/src/Ncqrs.Tests/Domain/AggregateRootMappedWithAttributesTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using FluentAssertions;
+using Ncqrs.Domain;
+using Ncqrs.Domain.Mapping;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace Ncqrs.Tests.Domain
+{
+    public class AggregateRootMappedWithAttributesTests
+    {
+        [Test]
+        public void Initializing_one_should_set_the_mapping_strategy_to_attributed_based()
+        {
+            var aggregateRoot = MockRepository.GenerateMock<AggregateRootMappedWithAttributes>();
+            var field = aggregateRoot.GetType().BaseType.BaseType.GetField("_mappingStrategy", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.GetField);
+            
+            var theStrategy = field.GetValue(aggregateRoot);
+            theStrategy.Should().BeOfType<AttributeBasedDomainEventHandlerMappingStrategy>();
+        }
+    }
+}
