b0d1c9b27b564eb88e9144a37c6d3b8cc5c1ba91
pj@born2code.net <pj@born2code.net>
2010-05-03 13:29:52 +0200

    Added extra DomainRepository test for Save method.

--- Framework/src/Ncqrs.Tests/Domain/Storage/DomainRepositoryTests.cs
+++ Framework/src/Ncqrs.Tests/Domain/Storage/DomainRepositoryTests.cs
@@ -8,6 +8,7 @@ using Ncqrs.Eventing.Storage;
 using Ncqrs.Eventing.ServiceModel.Bus;
 using Ncqrs.Domain;
 using System.Collections.Generic;
+using Ncqrs.Domain.Mapping;
 
 namespace Ncqrs.Tests.Domain.Storage
 {
@@ -19,19 +20,23 @@ namespace Ncqrs.Tests.Domain.Storage
         public class BarEvent : DomainEvent
         { }
 
-        public class MyAggregateRoot : AggregateRootMappedByConvention
+        public class MyAggregateRoot : AggregateRootMappedWithAttributes
         {
-            public List<DomainEvent> AppliedEvents = new List<DomainEvent>();
-
-            protected void OnFooEvent(FooEvent e)
+            public void Foo()
             {
-                AppliedEvents.Add(e);
+                var e = new FooEvent();
+                ApplyEvent(e);
             }
 
-            protected void OnBarEvent(BarEvent e)
+            public void Bar()
             {
-                AppliedEvents.Add(e);
+                var e = new BarEvent();
+                ApplyEvent(e);
             }
+
+            [EventHandlerAttribute]
+            private void CatchAllHandler(DomainEvent e)
+            {}
         }
 
         [Test]
@@ -41,20 +46,42 @@ namespace Ncqrs.Tests.Domain.Storage
             var bus = MockRepository.GenerateMock<IEventBus>();
             var loader = MockRepository.GenerateMock<IAggregateRootLoader>();
 
-            var repository = new DomainRepository(store, bus, loader);
-
             var aggId = Guid.NewGuid();
-            var eventsInTheStore = new DomainEvent[] {new FooEvent(), new BarEvent()};
+            var eventsInTheStore = new DomainEvent[] { new FooEvent(), new BarEvent() };
             var loadedAggregate = new MyAggregateRoot();
 
             store.Expect(s => s.GetAllEventsForEventSource(aggId)).Return(eventsInTheStore);
             loader.Expect(l => l.LoadAggregateRootFromEvents(typeof (MyAggregateRoot), eventsInTheStore)).Return(loadedAggregate);
 
+            var repository = new DomainRepository(store, bus, loader);
             var result = repository.GetById<MyAggregateRoot>(aggId);
 
             store.VerifyAllExpectations();
             loader.VerifyAllExpectations();
             result.Should().Be(loadedAggregate);
+        }
+
+        [Test]
+        public void Save_test()
+        {
+            using (var work = NcqrsEnvironment.Get<IUnitOfWorkFactory>().CreateUnitOfWork())
+            {
+                var store = MockRepository.GenerateMock<IEventStore>();
+                var bus = MockRepository.GenerateMock<IEventBus>();
+                var aggregate = new MyAggregateRoot();
+
+                aggregate.Foo();
+                aggregate.Bar();
+
+                store.Expect(s => s.Save(aggregate));
+                bus.Expect(b => b.Publish((IEnumerable<IEvent>) null)).IgnoreArguments();
+
+                var repository = new DomainRepository(store, bus);
+                repository.Save(aggregate);
+
+                bus.VerifyAllExpectations();
+                store.VerifyAllExpectations();
+            }
         }
     }
 }
