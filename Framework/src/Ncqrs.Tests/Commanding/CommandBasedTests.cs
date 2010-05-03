3a4e850cb7bfab563e0773dfe827f463729e0da5
pj@born2code.net <pj@born2code.net>
2010-05-03 16:35:12 +0200

    Small code cleanup and added tests for the CommandBased and TransactionalCommandExecutorWrapper classes.

---
+++ Framework/src/Ncqrs.Tests/Commanding/CommandBasedTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FluentAssertions;
+using Rhino.Mocks;
+using Ncqrs.Commanding;
+using NUnit.Framework;
+
+namespace Ncqrs.Tests.Commanding
+{
+    public class CommandBasedTests
+    {
+        [Test]
+        public void Constructing_a_new_event_base_it_should_call_the_GenerateNewId_method_from_the_generator_that_has_been_set_in_the_environment()
+        {
+            var generator = MockRepository.GenerateMock<IUniqueIdentifierGenerator>();
+            NcqrsEnvironment.SetDefault<IUniqueIdentifierGenerator>(generator);
+
+            var mock = MockRepository.GenerateStub<CommandBase>();
+
+            generator.AssertWasCalled(g => g.GenerateNewId());
+        }
+
+        [Test]
+        public void Constructing_a_new_event_base_it_should_set_the_event_identifier_to_identifier_that_has_been_given_from_the_IUniqueIdentifierGenerator_from_the_NcqrsEnvironment()
+        {
+            var identiefier = Guid.NewGuid();
+
+            var generator = MockRepository.GenerateStrictMock<IUniqueIdentifierGenerator>();
+            generator.Stub(g => g.GenerateNewId()).Return(identiefier);
+
+            NcqrsEnvironment.SetDefault<IUniqueIdentifierGenerator>(generator);
+
+            var mock = MockRepository.GenerateStub<CommandBase>();
+            mock.CommandIdentifier.Should().Be(identiefier);
+        }
+    }
+}
---
+++ Framework/src/Ncqrs.Tests/Commanding/TransactionalCommandExecutorWrapperTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Ncqrs.Commanding;
+using Rhino.Mocks;
+using Ncqrs.Commanding.CommandExecution;
+using NUnit.Framework;
+
+namespace Ncqrs.Tests.Commanding
+{
+    public class TransactionalCommandExecutorWrapperTests
+    {
+        [Test]
+        public void Executing_a_command_with_it_should_call_the_executor_that_was_set_at_construct()
+        {
+            var aExecutor = MockRepository.GenerateMock<ICommandExecutor>();
+            var aCommand = MockRepository.GenerateMock<ICommand>();
+            var theWrapper = new TransactionalCommandExecutorWrapper(aExecutor);
+
+            theWrapper.Execute(aCommand);
+
+            aExecutor.AssertWasCalled(e=>e.Execute(aCommand));
+        }
+    }
+}
