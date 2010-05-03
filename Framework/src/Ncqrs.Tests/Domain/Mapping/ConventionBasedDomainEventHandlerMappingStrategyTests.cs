af692aa254fb97203afea88ff1913255cf8d05d7
pj@born2code.net <pj@born2code.net>
2010-05-03 10:42:35 +0200

    Added tests for InvalidEventHandlerMappingException and ConventionBasedDomainEventHandlerMappingStrategy class.

--- Framework/src/Ncqrs.Tests/Domain/Mapping/ConventionBasedDomainEventHandlerMappingStrategyTests.cs
+++ Framework/src/Ncqrs.Tests/Domain/Mapping/ConventionBasedDomainEventHandlerMappingStrategyTests.cs
@@ -1,42 +1,37 @@
 ﻿using System;
 using System.Linq;
 using FluentAssertions;
-using NUnit.Framework;
 using Ncqrs.Domain;
 using Ncqrs.Domain.Mapping;
+using NUnit.Framework;
 
 namespace Ncqrs.Tests.Domain.Mapping
 {
-    public class AttributeBasedDomainEventHandlerMappingStrategyTests
+    public class ConventionBasedDomainEventHandlerMappingStrategyTests
     {
-        public class IlligalStaticMethodTarget
-            : AggregateRoot
+        public class IlligalStaticMethodTarget : AggregateRoot
         {
-            [EventHandler]
-            public static void MyEventHandlerMethod(DomainEvent e)
-            {}
+            public static void OnDomainEvent(DomainEvent e)
+            { }
         }
 
         public class NoParameterMethodTarget : AggregateRoot
         {
-            [EventHandler]
-            public void MyEventHandlerMethod()
+            public void OnMyEvent()
             {
             }
         }
 
         public class MoreThenOneParameterMethodTarget : AggregateRoot
         {
-            [EventHandler]
-            public void MyEventHandlerMethod(DomainEvent e1, DomainEvent e2)
+            public void OnDomainEvent(DomainEvent e1, DomainEvent e2)
             {
             }
         }
 
         public class NotADomainEventTarget : AggregateRoot
         {
-            [EventHandler]
-            public void MyEventHandlerMethod(String e)
+            public void OnDomainEvent(String e)
             {
             }
         }
@@ -52,103 +47,93 @@ namespace Ncqrs.Tests.Domain.Mapping
             public int ProtectedEventHandlerInvokeCount;
             public int InternalEventHandlerInvokeCount;
             public int PrivateEventHandlerInvokeCount;
-            public int CatchAllEventHandlerInvokeCount;
 
-            [EventHandler]
-            public void PublicEventHandler(PublicEvent e)
+            public void OnPublicEvent(PublicEvent e)
             {
                 PublicEventHandlerInvokeCount++;
             }
 
-            [EventHandler]
-            private void ProtectedEventHandler(ProtectedEvent e)
+            private void OnProtectedEvent(ProtectedEvent e)
             {
                 ProtectedEventHandlerInvokeCount++;
             }
 
-            [EventHandler]
-            private void InternalEventHandler(InternalEvent e)
+            private void OnInternalEvent(InternalEvent e)
             {
                 InternalEventHandlerInvokeCount++;
             }
 
-            [EventHandler]
-            private void PrivateEventHandler(PrivateEvent e)
+            private void OnPrivateEvent(PrivateEvent e)
             {
                 PrivateEventHandlerInvokeCount++;
             }
-
-            [EventHandler]
-            private  void CatchAllEventHandler(DomainEvent e)
-            {
-                CatchAllEventHandlerInvokeCount++;
-            }
         }
 
         [Test]
-        public void It_should_throw_an_exception_when_mapped_method_is_static()
+        public void It_should_skip_when_mapped_method_is_static()
         {
             var aggregate = new IlligalStaticMethodTarget();
-            var mapping = new AttributeBasedDomainEventHandlerMappingStrategy();
+            var mapping = new ConventionBasedDomainEventHandlerMappingStrategy();
 
-            Action act = ()=>mapping.GetEventHandlersFromAggregateRoot(aggregate);
+            var handlers = mapping.GetEventHandlersFromAggregateRoot(aggregate);
 
-            act.ShouldThrow<InvalidEventHandlerMappingException>();
+            handlers.Should().BeEmpty();
         }
 
         [Test]
-        public void It_should_throw_an_exception_when_mapped_method_does_not_have_a_parameter()
+        public void It_should_skip_when_mapped_method_does_not_have_a_parameter()
         {
             var aggregate = new NoParameterMethodTarget();
-            var mapping = new AttributeBasedDomainEventHandlerMappingStrategy();
+            var mapping = new ConventionBasedDomainEventHandlerMappingStrategy();
 
-            Action act = () => mapping.GetEventHandlersFromAggregateRoot(aggregate);
+            var handlers = mapping.GetEventHandlersFromAggregateRoot(aggregate);
 
-            act.ShouldThrow<InvalidEventHandlerMappingException>();
+            handlers.Should().BeEmpty();
         }
 
         [Test]
-        public void It_should_throw_an_exception_when_mapped_method_does_have_more_then_one_parameter()
+        public void It_should_skip_when_mapped_method_does_have_more_then_one_parameter()
         {
             var aggregate = new MoreThenOneParameterMethodTarget();
-            var mapping = new AttributeBasedDomainEventHandlerMappingStrategy();
+            var mapping = new ConventionBasedDomainEventHandlerMappingStrategy();
 
-            Action act = () => mapping.GetEventHandlersFromAggregateRoot(aggregate);
+            var handlers = mapping.GetEventHandlersFromAggregateRoot(aggregate);
 
-            act.ShouldThrow<InvalidEventHandlerMappingException>();
+            handlers.Should().BeEmpty();
         }
 
         [Test]
-        public void It_should_throw_an_exception_when_mapped_method_does_not_have_a_DomainEvent_as_parameter()
+        public void It_should_skip_when_mapped_method_does_not_have_a_DomainEvent_as_parameter()
         {
             var aggregate = new NotADomainEventTarget();
-            var mapping = new AttributeBasedDomainEventHandlerMappingStrategy();
+            var mapping = new ConventionBasedDomainEventHandlerMappingStrategy();
 
-            Action act = () => mapping.GetEventHandlersFromAggregateRoot(aggregate);
+            var handlers = mapping.GetEventHandlersFromAggregateRoot(aggregate);
 
-            act.ShouldThrow<InvalidEventHandlerMappingException>();
+            handlers.Should().BeEmpty();
         }
 
         [Test]
         public void It_should_map_the_mapped_events()
         {
             var aggregate = new GoodTarget();
-            var mapping = new AttributeBasedDomainEventHandlerMappingStrategy();
+            var mapping = new ConventionBasedDomainEventHandlerMappingStrategy();
 
             var handlers = mapping.GetEventHandlersFromAggregateRoot(aggregate);
 
             handlers.Count().Should().Be(5);
+            handlers.Should().OnlyHaveUniqueItems();
         }
 
         [Test]
         public void It_should_create_the_correct_event_handlers()
         {
             var aggregate = new GoodTarget();
-            var mapping = new AttributeBasedDomainEventHandlerMappingStrategy();
+            var mapping = new ConventionBasedDomainEventHandlerMappingStrategy();
 
             var handlers = mapping.GetEventHandlersFromAggregateRoot(aggregate);
 
-            foreach(var handler in handlers)
+            foreach (var handler in handlers)
             {
                 handler.HandleEvent(new GoodTarget.PublicEvent());
                 handler.HandleEvent(new GoodTarget.ProtectedEvent());
@@ -160,7 +145,6 @@ namespace Ncqrs.Tests.Domain.Mapping
             aggregate.ProtectedEventHandlerInvokeCount.Should().Be(1);
             aggregate.InternalEventHandlerInvokeCount.Should().Be(1);
             aggregate.PrivateEventHandlerInvokeCount.Should().Be(1);
-            aggregate.CatchAllEventHandlerInvokeCount.Should().Be(4);
         }
     }
 }
---
+++ Framework/src/Ncqrs.Tests/Domain/Mapping/InvalidEventHandlerMappingExceptionTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using FluentAssertions;
+using Ncqrs.Domain.Mapping;
+using NUnit.Framework;
+
+namespace Ncqrs.Tests.Domain.Mapping
+{
+    public class InvalidEventHandlerMappingExceptionTests
+    {
+        [Test]
+        public void Constructing_an_instance_should_initialize_the_message()
+        {
+            String message = "Hello world";
+
+            var target = new InvalidEventHandlerMappingException(message);
+
+            target.Message.Should().Be(message);
+        }
+
+        [Test]
+        public void Constructing_an_instance_should_initialize_the_inner_exception()
+        {
+            String aMessage = "Hello world";
+            var theInnerException = new Exception();
+
+            var target = new InvalidEventHandlerMappingException(aMessage, theInnerException);
+
+            target.InnerException.Should().Be(theInnerException);
+        }
+
+        [Test]
+        public void It_should_be_serializable()
+        {
+            var aMessage = "Hello world";
+
+            var theException = new InvalidEventHandlerMappingException(aMessage);
+            InvalidEventHandlerMappingException deserializedException = null;
+
+            using (var buffer = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(buffer, theException);
+
+                buffer.Seek(0, SeekOrigin.Begin);
+                deserializedException = (InvalidEventHandlerMappingException)formatter.Deserialize(buffer);
+            }
+
+            deserializedException.Should().NotBeNull();
+        }
+    }
+}
