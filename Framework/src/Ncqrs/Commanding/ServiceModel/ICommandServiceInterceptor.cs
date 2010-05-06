24ad32d3089e4c5a5b3c7ed3e92e966e9ced9107
pj@born2code.net <pj@born2code.net>
2010-05-06 11:57:37 +0200

    Added interception model to the command service. This allows people to create intercepters for cross cutting concerns like logging and authorization.

---
+++ Framework/src/Ncqrs/Commanding/ServiceModel/ICommandServiceInterceptor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Ncqrs.Commanding.ServiceModel
+{
+    public interface ICommandServiceInterceptor
+    {
+        void OnBeforeExecution(CommandServiceExecutionContext context);
+        void OnAfterExecution(CommandServiceExecutionContext context);
+    }
+}
--- Framework/src/Ncqrs/Commanding/ServiceModel/InProcessCommandService.cs
+++ Framework/src/Ncqrs/Commanding/ServiceModel/InProcessCommandService.cs
@@ -1,75 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Reflection;
 using Ncqrs.Commanding.CommandExecution;
 
 namespace Ncqrs.Commanding.ServiceModel
 {
     /// <summary>
-    /// A dispatcher that dispatch command objects to their appropriate command executor. Command executors can register and
-    /// unregister to specific command types. Only a single executor may be subscribed for a single type of command at any time.
+    /// A in process command service.
     /// </summary>
-    public class InProcessCommandService : CommandExecutionDispatcher, ICommandService
+    public class InProcessCommandService : CommandServiceBase
     {
-        private readonly Dictionary<Type, ICommandExecutor> _executors = new Dictionary<Type, ICommandExecutor>();
-        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-
-        /// <summary>
-        /// Registers the executor for the specified command type. The executor will be called for every command of the specified type.
-        /// </summary>
-        /// <typeparam name="TCommand">The type of the command.</typeparam>
-        /// <param name="executor">The executor that will be called for every command of the specified type.</param>
-        /// <exception cref="ArgumentNullException">Occurs when the <i>commandType</i> or <i>executor</i> was a <c>null</c> dereference.</exception>
         public void RegisterExecutor<TCommand>(ICommandExecutor executor) where TCommand : ICommand
         {
-            RegisterExecutor(typeof(TCommand), executor);
+            base.RegisterExecutor<TCommand>(executor);
         }
 
-        /// <summary>
-        /// Registers the executor for the specified command type. The executor will be called for every command of the specified type.
-        /// </summary>
-        /// <param name="commandType">Type of the command.</param>
-        /// <param name="executor">The executor that will be called for every command of the specified type.</param>
-        /// <exception cref="ArgumentNullException">Occurs when the <i>commandType</i> or <i>executor</i> was a <c>null</c> dereference.</exception>
         public void RegisterExecutor(Type commandType, ICommandExecutor executor)
         {
-            _executors.Add(commandType, executor);
+            base.RegisterExecutor(commandType, executor);
         }
 
-
-        /// <summary>
-        /// Unregisters the executor of the specified command type. The executor will not be called any more.
-        /// </summary>
-        /// <param name="commandType">Type of the command.</param>
-        /// <param name="executor">The executor to unregister.</param>
-        /// <exception cref="ArgumentNullException">Occurs when the <i>commandType</i> or <i>executor</i> was a <c>null</c> dereference.</exception>
-        /// <exception cref="InvalidOperationException">Occurs when the <i>executor</i> is not the same as the registered executor for the specified command type.</exception>
         public void UnregisterExecutor(Type commandType, ICommandExecutor executor)
         {
-            ICommandExecutor registeredExecutor = null;
-
-            if (_executors.TryGetValue(commandType, out registeredExecutor))
-            {
-                if (executor != registeredExecutor)
-                    throw new InvalidOperationException("The specified executor does not match with the registered executor for command type " + commandType.FullName + ".");
-
-                _executors.Remove(commandType);
-            }
-        }
-
-        /// <summary>
-        /// Gets the command executor for command.
-        /// </summary>
-        /// <param name="commandType">Type of the command.</param>
-        /// <returns>
-        /// A command executor to use to execute the command or <c>null</c> if not found.
-        /// </returns>
-        protected override ICommandExecutor GetCommandExecutorForCommand(Type commandType)
-        {
-            ICommandExecutor result;
-            _executors.TryGetValue(commandType, out result);
-
-            return result;
+            base.UnregisterExecutor(commandType, executor);
         }
     }
 }
