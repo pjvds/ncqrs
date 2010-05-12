b3a8a6ed0ee61411a4c1be40dd5213cda9112104
pj@born2code.net <pj@born2code.net>
2010-05-12 08:33:12 +0200

    Added code sample to AddProductToShoppingCartExecutor.

--- Framework/src/Ncqrs/Commanding/CommandExecution/CommandExecutorBase.cs
+++ Framework/src/Ncqrs/Commanding/CommandExecution/CommandExecutorBase.cs
@@ -3,20 +3,25 @@ using Ncqrs.Domain;
 
 namespace Ncqrs.Commanding.CommandExecution
 {
-    public class AddProductToShoppingCartExecutor : CommandExecutorBase<AddProductToShoppingCart>
-    {
-        protected override void ExecuteInContext(IUnitOfWorkContext context, AddProductToShoppingCart command)
-        {
-            var shoppingCart = context.GetById<ShoppingCart>(command.ShoppingCartId);
-            shoppingCart.AddProduct(command.ProductId, command.Amount);
-
-            context.Accept();
-        }
-    }
-
     /// <summary>
     /// Represents a command executor.
     /// </summary>
+    /// <code lang="c#">
+    /// public class AddProductToShoppingCartExecutor : CommandExecutorBase
+    /// {
+    ///     protected override void ExecuteInContext(IUnitOfWorkContext context, AddProductToShoppingCart command)
+    ///     {
+    ///         // Get the shopping cart.
+    ///         var shoppingCart = context.GetById(command.ShoppingCartId);
+    /// 
+    ///         // Add the product to the shopping cart.
+    ///         shoppingCart.AddProduct(command.ProductId, command.Amount);
+    ///
+    ///         // Accept all the work we just did.
+    ///         context.Accept();
+    ///     }
+    /// }
+    /// </code>
     /// <typeparam name="TCommand">The type of the commands to execute.</typeparam>
     public abstract class CommandExecutorBase<TCommand> : ICommandExecutor<TCommand> where TCommand : ICommand
     {
@@ -37,6 +42,24 @@ namespace Ncqrs.Commanding.CommandExecution
         /// Executes the command withing an unit of work context.
         /// <remarks>Make sure you call <see cref="IUnitOfWorkContext.Accept"/> to accept the changes that has been made in the context.</remarks>
         /// </summary>
+        /// <example>
+        /// <code lang="c#">
+        /// public class AddProductToShoppingCartExecutor : CommandExecutorBase
+        /// {
+        ///     protected override void ExecuteInContext(IUnitOfWorkContext context, AddProductToShoppingCart command)
+        ///     {
+        ///         // Get the shopping cart.
+        ///         var shoppingCart = context.GetById(command.ShoppingCartId);
+        /// 
+        ///         // Add the product to the shopping cart.
+        ///         shoppingCart.AddProduct(command.ProductId, command.Amount);
+        ///
+        ///         // Accept all the work we just did.
+        ///         context.Accept();
+        ///     }
+        /// }
+        /// </code>
+        /// </example>
         /// <param name="context">The work context.</param>
         /// <param name="command">The command to execute.</param>
         protected abstract void ExecuteInContext(IUnitOfWorkContext context, TCommand command);
