97b9110a7a1b97d6fb2c4b8b482346b0ab5ad9dc
pj@born2code.net <pj@born2code.net>
2010-05-12 08:12:17 +0200

    Added the command as a parameter for the ExecuteInContext method.

--- Framework/src/Ncqrs/Commanding/CommandExecution/CommandExecutorBase.cs
+++ Framework/src/Ncqrs/Commanding/CommandExecution/CommandExecutorBase.cs
@@ -3,6 +3,17 @@ using Ncqrs.Domain;
 
 namespace Ncqrs.Commanding.CommandExecution
 {
+    public class AddProductToShoppingCartExecutor : CommandExecutorBase<AddProductToShoppingCart>
+    {
+        protected override void ExecuteInContext(IUnitOfWorkContext context, AddProductToShoppingCart command)
+        {
+            var shoppingCart = context.GetById<ShoppingCart>(command.ShoppingCartId);
+            shoppingCart.AddProduct(command.ProductId, command.Amount);
+
+            context.Accept();
+        }
+    }
+
     /// <summary>
     /// Represents a command executor.
     /// </summary>
@@ -18,15 +29,16 @@ namespace Ncqrs.Commanding.CommandExecution
         {
             using (var work = NcqrsEnvironment.Get<IUnitOfWorkFactory>().CreateUnitOfWork())
             {
-                ExecuteWithingUnitOfWorkContext(work);
+                ExecuteInContext(work, command);
             }
         }
 
         /// <summary>
         /// Executes the command withing an unit of work context.
-        /// <remarks>Make sure you call <see cref="IUnitOfWork.Accept"/> to accept the changes that has been made in the context.</remarks>
+        /// <remarks>Make sure you call <see cref="IUnitOfWorkContext.Accept"/> to accept the changes that has been made in the context.</remarks>
         /// </summary>
-        /// <param name="work">The work.</param>
-        protected abstract void ExecuteWithingUnitOfWorkContext(IUnitOfWorkContext work);
+        /// <param name="context">The work context.</param>
+        /// <param name="command">The command to execute.</param>
+        protected abstract void ExecuteInContext(IUnitOfWorkContext context, TCommand command);
     }
 }
