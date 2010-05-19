ab13f28c83aa217c21b662ed80fa0412dc264d52
pj@born2code.net <pj@born2code.net>
2010-05-19 16:46:00 +0200

    Added a few lines of Xml documentation.

--- Framework/src/Ncqrs/Eventing/ISnapshotable.cs
+++ Framework/src/Ncqrs/Eventing/ISnapshotable.cs
@@ -6,16 +6,16 @@ using System.Diagnostics.Contracts;
 namespace Ncqrs.Eventing
 {
     /// <summary>
-    /// This interface flags an object beeing <i>mementoable</i>. This means that the
-    /// state of the object could be saved to an <see cref="IMemento"/> object
+    /// This interface flags an object beeing <i>snapshotable</i>. This means that the
+    /// state of the object could be saved to an <see cref="ISnapshot"/> object
     /// and restored from a the from the same class.
     /// This is used to prevent building <see cref="AggregateRoot"/>'s from the ground up.
     /// </summary>
     [ContractClass(typeof(SnapshotableContracts<>))]
     public interface ISnapshotable<TSnapshot> : IEventSource where TSnapshot : ISnapshot
     {
-        void RestoreFromSnapshot(TSnapshot snapshot);
         TSnapshot CreateSnapshot();
+        void RestoreFromSnapshot(TSnapshot snapshot);
     }
 
     [ContractClassFor(typeof(ISnapshotable<>))]
