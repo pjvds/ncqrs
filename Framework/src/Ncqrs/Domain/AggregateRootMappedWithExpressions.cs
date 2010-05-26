01dfe4265cf0bfd231b2db6c77c9e316702e0096
mdirksma@gmail.com <mdirksma@gmail.com>
2010-05-26 15:51:15 +0200

    first (simple) draft of stongly typed mapping

--- Framework/src/Ncqrs/Domain/AggregateRootMappedWithExpressions.cs
+++ Framework/src/Ncqrs/Domain/AggregateRootMappedWithExpressions.cs
@@ -4,14 +4,32 @@ using System.Linq;
 using System.Text;
 using Ncqrs.Domain.Mapping;
 using System.Linq.Expressions;
+using System.Reflection;
 
 namespace Ncqrs.Domain
 {
     public abstract class AggregateRootMappedWithExpressions : MappedAggregateRoot<AggregateRootMappedWithExpressions>
     {
+        private IList<IExpressionHandler> _mappinghandlers = new List<IExpressionHandler>();
+
+        internal IEnumerable<IExpressionHandler> MappingHandlers
+        {
+            get { return _mappinghandlers; }
+        }
+
         protected AggregateRootMappedWithExpressions() 
             : base(new ExpressionBasedDomainEventHandlerMappingStrategy())
-        {}
+        {
+            InitMappingRules();
+        }
+        
+        protected IExpressionHandler<T> Map<T>() where T : DomainEvent
+        {
+            var handler = new ExpressionHandlerImpl<T>();
+            _mappinghandlers.Add(handler);
+
+            return handler;
+        }
 
         public abstract void InitMappingRules();
     }
--- Framework/src/Ncqrs/Domain/Mapping/ExpressionBasedDomainEventHandlerMappingStrategy.cs
+++ Framework/src/Ncqrs/Domain/Mapping/ExpressionBasedDomainEventHandlerMappingStrategy.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics.Contracts;
+using System.Reflection;
 
 namespace Ncqrs.Domain.Mapping
 {
@@ -11,9 +13,33 @@ namespace Ncqrs.Domain.Mapping
     /// </summary>
     public class ExpressionBasedDomainEventHandlerMappingStrategy : IDomainEventHandlerMappingStrategy<AggregateRootMappedWithExpressions>
     {
+        private static IDomainEventHandler CreateHandlerForMethod(AggregateRoot aggregateRoot, MethodInfo method, bool exact)
+        {
+            Type firstParameterType = method.GetParameters().First().ParameterType;
+            
+            Action<DomainEvent> handler = e => method.Invoke(aggregateRoot, new object[] { e });
+            return new TypeThresholdedActionBasedDomainEventHandler(handler, firstParameterType, exact);
+        }
+
         public IEnumerable<IDomainEventHandler> GetEventHandlersFromAggregateRoot(AggregateRootMappedWithExpressions aggregateRoot)
         {
-            throw new NotImplementedException("");
+            Contract.Requires<ArgumentNullException>(aggregateRoot != null, "The aggregateRoot cannot be null.");
+            
+            var handlers = new List<IDomainEventHandler>();
+
+            foreach (IExpressionHandler mappinghandler in aggregateRoot.MappingHandlers)
+            {
+                if (mappinghandler.ActionMethodInfo.IsStatic)
+                {
+                    var message = String.Format("The method {0}.{1} could not be mapped as an event handler, since it is static.", mappinghandler.ActionMethodInfo.DeclaringType.Name, mappinghandler.ActionMethodInfo.Name);
+                    throw new InvalidEventHandlerMappingException(message);
+                }
+
+                var handler = CreateHandlerForMethod(aggregateRoot, mappinghandler.ActionMethodInfo, mappinghandler.Exact);
+                handlers.Add(handler);
+            }
+
+            return handlers;
         }
     }
 }
