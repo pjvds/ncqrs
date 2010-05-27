4094b854f3a2c5fd6300435287fdced73a530a06
mdirksma@gmail.com <mdirksma@gmail.com>
2010-05-27 08:11:02 +0200

    added first unit tests and IExpressionHandler, IExpressionHandlerImpl classes

---
+++ Framework/src/Ncqrs/Domain/Mapping/IExpressionHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace Ncqrs.Domain.Mapping
+{
+    public interface IExpressionHandler
+    {
+        bool Exact { get; }
+        MethodInfo ActionMethodInfo { get; }
+    }
+
+    public interface IExpressionHandler<T> : IExpressionHandler where T : DomainEvent
+    {
+        IExpressionHandler<T> ToHandler(Action<T> act);
+        IExpressionHandler<T> MatchExact();
+    }
+}
