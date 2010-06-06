9a211e7a1d0032d9d4ae991ff2471ea64d150617
pj@born2code.net <pj@born2code.net>
2010-06-06 14:05:09 +0200

    Fixed bug in SimpleSqlEventStore concurrency check.

--- Framework/src/Ncqrs/Eventing/Storage/SQL/TableCreationScript.sql
+++ Framework/src/Ncqrs/Eventing/Storage/SQL/TableCreationScript.sql
@@ -1,3 +1,10 @@
-﻿CREATE TABLE [dbo].[Events]([EventSourceId] [uniqueidentifier] NOT NULL, [Sequence] [bigint], [TimeStamp] [datetime] NOT NULL, [Data] [varbinary](max) NOT NULL, [Name] [varchar](max) NOT NULL) ON [PRIMARY];
+﻿CREATE TABLE 
+	[dbo].[Events]
+	(
+		[EventSourceId] [uniqueidentifier] NOT NULL, [Sequence] [bigint], [TimeStamp] [datetime] NOT NULL, 
+		[Data] [varbinary](max) NOT NULL, [Name] [varchar](max) NOT NULL
+	)
+	ON [PRIMARY];
+	GO;
 CREATE TABLE [dbo].[EventSources]([Id] [uniqueidentifier] NOT NULL, [Type] [nvarchar](255) NOT NULL, [Version] [int] NOT NULL) ON [PRIMARY];
 CREATE TABLE [dbo].[Snaptshots]([EventSourceId] [uniqueidentifier] NOT NuLL, [Version] [bigint], [TimeStamp] [datetime] NOt NULL, [Data] [varbinary](max) NOT NULL) ON [PRIMARY]
