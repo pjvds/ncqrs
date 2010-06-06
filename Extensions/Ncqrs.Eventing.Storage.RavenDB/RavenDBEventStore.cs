aa36060e9cef808c8fa144e2f621210aabbb61aa
szymon.pobiega@gmail.com <szymon.pobiega@gmail.com>
2010-06-06 14:58:57 +0200

    Unit tests done and working on slightly modified version of RavenDB

---
+++ Extensions/Ncqrs.Eventing.Storage.RavenDB/RavenDBEventStore.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Ncqrs.Domain;
+using Raven.Client;
+using Raven.Client.Document;
+
+namespace Ncqrs.Eventing.Storage.RavenDB
+{
+    public class RavenDBEventStore : IEventStore
+    {
+        private readonly IDocumentStore _documentStore;
+
+        public RavenDBEventStore(string ravenUrl)
+        {
+            _documentStore = new DocumentStore
+            {
+                Url = ravenUrl,                
+                Conventions = CreateConventions()
+            }.Initialise(); 
+        }
+
+        public RavenDBEventStore(DocumentStore externalDocumentStore)
+        {
+            externalDocumentStore.Conventions = CreateConventions();
+            _documentStore = externalDocumentStore;            
+        }
+
+        private static DocumentConvention CreateConventions()
+        {
+            return new DocumentConvention
+            {
+                JsonContractResolver = new PropertiesOnlyContractResolver(),
+                FindTypeTagName = FindTypeTagName,
+                NewDocumentETagGenerator = GenerateETag
+            };
+        }
+
+        private static Guid? GenerateETag(object entity)
+        {
+            var sourcedEvent = entity as StoredEvent;
+            if (sourcedEvent != null)
+            {
+                return Guid.NewGuid();
+            }
+            return null;
+        }
+
+        private static string FindTypeTagName(Type type)
+        {
+            return type == typeof(StoredEvent)
+                ? "Events" 
+                : "Snapshots";
+        }        
+
+        public IEnumerable<ISourcedEvent> GetAllEvents(Guid id)
+        {
+            using (var session = _documentStore.OpenSession())
+            {
+                return session.Query<StoredEvent>()
+                    .WaitForNonStaleResults()
+                    .Where(x => x.EventSourceId == id)
+                    .ToList().OrderBy(x => x.EventSequence)
+                    .Select(x => x.Data);                   
+            }
+        }
+
+        public IEnumerable<ISourcedEvent> GetAllEventsSinceVersion(Guid id, long version)
+        {
+            using (var session = _documentStore.OpenSession())
+            {
+                return session.Query<StoredEvent>()
+                    .WaitForNonStaleResults()
+                    .Where(x => x.EventSourceId == id)
+                    .Where(x => x.EventSequence >= version)
+                    .ToList().OrderBy(x => x.EventSequence)
+                    .Select(x => x.Data);                   
+            }
+        }
+
+        public void Save(IEventSource source)
+        {
+            try
+            {
+                using (var session = _documentStore.OpenSession())
+                {
+                    session.UseOptimisticConcurrency = true;
+                    foreach (var sourcedEvent in source.GetUncommittedEvents())
+                    {
+                        session.Store(new StoredEvent
+                        {
+                            Data = sourcedEvent,
+                            EventSequence = sourcedEvent.EventSequence,
+                            EventSourceId = sourcedEvent.EventSourceId,
+                            Id = sourcedEvent.EventSourceId + "/" + sourcedEvent.EventSequence
+                        });
+                    }
+                    session.SaveChanges();
+                }
+            }
+            catch (Raven.Database.Exceptions.ConcurrencyException)
+            {
+                throw new ConcurrencyException(source.Id, source.Version);
+            }
+        }        
+    }
+}
---
+++ Extensions/src/Ncqrs.Eventing.Storage.RavenDB.Tests/RavenDBEventStoreTests.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using FluentAssertions;
+using log4net.Config;
+using Ncqrs.Domain;
+using NUnit.Framework;
+using Raven.Client;
+using Raven.Client.Document;
+using Rhino.Mocks;
+
+namespace Ncqrs.Eventing.Storage.RavenDB.Tests
+{
+    [TestFixture]
+    public class RavenDBEventStoreTests
+    {
+        [Serializable]
+        public class CustomerCreatedEvent : DomainEvent
+        {
+            public CustomerCreatedEvent(Guid eventIdentifier, Guid aggregateRootId, long eventSequence, DateTime eventTimeStamp, string name, int age)
+                : base(eventIdentifier, aggregateRootId, eventSequence, eventTimeStamp)
+            {
+                Name = name;
+                Age = age;
+            }
+
+            public string Name
+            { get; set; }
+
+            public int Age
+            {
+                get;
+                set;
+            }
+
+            public override bool Equals(object obj)
+            {
+                var other = obj as CustomerCreatedEvent;
+                if (other == null)
+                {
+                    return false;
+                }
+                bool result = EventIdentifier.Equals(other.EventIdentifier)
+                       && AggregateRootId.Equals(other.AggregateRootId)
+                       && EventSequence.Equals(other.EventSequence)
+                       //&& EventTimeStamp.Equals(other.EventTimeStamp)
+                       && Name.Equals(other.Name)
+                       && Age.Equals(other.Age);
+                return result;
+            }
+        }
+
+        [Serializable]
+        public class CustomerNameChanged : DomainEvent
+        {
+            public Guid CustomerId
+            {
+                get
+                {
+                    return AggregateRootId;
+                }
+            }
+
+            public string NewName
+            { get; set; }
+
+            public CustomerNameChanged(Guid eventIdentifier, Guid aggregateRootId, long eventSequence, DateTime eventTimeStamp, string newName)
+                : base(eventIdentifier, aggregateRootId, eventSequence, eventTimeStamp)
+            {
+                NewName = newName;
+            }
+
+            public override bool Equals(object obj)
+            {
+                var other = obj as CustomerNameChanged;
+                if (other == null)
+                {
+                    return false;
+                }
+                bool result = EventIdentifier.Equals(other.EventIdentifier)
+                       && AggregateRootId.Equals(other.AggregateRootId)
+                       && EventSequence.Equals(other.EventSequence)
+                       //&& EventTimeStamp.Equals(other.EventTimeStamp)
+                       && CustomerId.Equals(other.CustomerId)
+                       && NewName.Equals(other.NewName);
+                return result;
+            }
+        }
+
+        private DocumentStore _documentStore;
+        private string path;
+
+        [SetUp]
+        public void SetUpDocumentStore()
+        {
+            XmlConfigurator.Configure();
+            //_documentStore = ConnectToDocumentStore();
+            _documentStore = NewDocumentStore();
+        }
+
+        [TearDown]
+        public void TearDownDocumentStore()
+        {
+            _documentStore.Dispose();
+            if (path != null)
+            {
+                Directory.Delete(path, true);
+            }
+        }
+
+        private static DocumentStore ConnectToDocumentStore()
+        {
+            var documentStore = new DocumentStore
+            {
+                Url = "http://localhost:8080"
+            };
+            documentStore.Initialise();
+            return documentStore;
+        }
+
+        private DocumentStore NewDocumentStore()
+        {
+            path = Path.GetDirectoryName(Assembly.GetAssembly(typeof(RavenDBEventStoreTests)).CodeBase);
+            path = Path.Combine(path, "TestDb").Substring(6);
+            var documentStore = new DocumentStore
+            {
+                Configuration =
+                {
+                    DataDirectory = path,
+                    RunInUnreliableYetFastModeThatIsNotSuitableForProduction = true
+                }
+            };
+            documentStore.Initialise();
+            return documentStore;
+        }
+
+        [Test]
+        public void Saving_event_source_should_succeed()
+        {
+            var targetStore = new RavenDBEventStore(_documentStore);
+            var id = Guid.NewGuid();
+
+            int sequenceCounter = 0;
+
+            var events = new ISourcedEvent[]
+                             {
+                                 new CustomerCreatedEvent(Guid.NewGuid(), id, sequenceCounter++, DateTime.UtcNow, "Foo",
+                                                          35),
+                                 new CustomerNameChanged(Guid.NewGuid(), id, sequenceCounter++, DateTime.UtcNow,
+                                                         "Name" + sequenceCounter),
+                                 new CustomerNameChanged(Guid.NewGuid(), id, sequenceCounter++, DateTime.UtcNow,
+                                                         "Name" + sequenceCounter),
+                                 new CustomerNameChanged(Guid.NewGuid(), id, sequenceCounter++, DateTime.UtcNow,
+                                                         "Name" + sequenceCounter)
+                             };
+
+            var eventSource = MockRepository.GenerateMock<IEventSource>();
+            eventSource.Stub(e => e.Id).Return(id);
+            eventSource.Stub(e => e.InitialVersion).Return(0);
+            eventSource.Stub(e => e.Version).Return(events.Length);
+            eventSource.Stub(e => e.GetUncommittedEvents()).Return(events);
+
+            targetStore.Save(eventSource);
+        }
+
+        [Test]
+        public void Saving_event_source_while_there_is_a_newer_event_source_should_throw_concurency_exception()
+        {
+            var targetStore = new RavenDBEventStore(_documentStore);
+            var id = Guid.NewGuid();
+
+            int sequenceCounter = 0;
+
+            var events = new ISourcedEvent[]
+                             {
+                                 new CustomerCreatedEvent(Guid.NewGuid(), id, sequenceCounter++, DateTime.UtcNow, "Foo",
+                                                          35),
+                                 new CustomerNameChanged(Guid.NewGuid(), id, sequenceCounter++, DateTime.UtcNow,
+                                                         "Name" + sequenceCounter)
+                             };
+            
+            var eventSource = MockRepository.GenerateMock<IEventSource>();
+            eventSource.Stub(e => e.Id).Return(id).Repeat.Any();
+            eventSource.Stub(e => e.InitialVersion).Return(0).Repeat.Any();
+            eventSource.Stub(e => e.Version).Return(events.Length).Repeat.Any();
+            eventSource.Stub(e => e.GetUncommittedEvents()).Return(events).Repeat.Any();
+
+            targetStore.Save(eventSource);
+            
+            Action act = () => targetStore.Save(eventSource);
+            act.ShouldThrow<ConcurrencyException>();
+        }
+
+        [Test]
+        public void Retrieving_all_events_should_return_the_same_as_added()
+        {
+            var targetStore = new RavenDBEventStore(_documentStore);
+            var id = Guid.NewGuid();
+
+            int sequenceCounter = 0;
+
+            var events = new ISourcedEvent[]
+                             {
+                                 new CustomerCreatedEvent(Guid.NewGuid(), id, sequenceCounter++, DateTime.UtcNow, "Foo",
+                                                          35),
+                                 new CustomerNameChanged(Guid.NewGuid(), id, sequenceCounter++, DateTime.UtcNow,
+                                                         "Name" + sequenceCounter),
+                                 new CustomerNameChanged(Guid.NewGuid(), id, sequenceCounter++, DateTime.UtcNow,
+                                                         "Name" + sequenceCounter),
+                                 new CustomerNameChanged(Guid.NewGuid(), id, sequenceCounter++, DateTime.UtcNow,
+                                                         "Name" + sequenceCounter)
+                             };
+
+            var eventSource = MockRepository.GenerateMock<IEventSource>();
+            eventSource.Stub(e => e.Id).Return(id);
+            eventSource.Stub(e => e.InitialVersion).Return(0);
+            eventSource.Stub(e => e.Version).Return(events.Length);
+            eventSource.Stub(e => e.GetUncommittedEvents()).Return(events);
+
+            targetStore.Save(eventSource);
+
+            var result = targetStore.GetAllEvents(id);
+            result.Count().Should().Be(events.Length);
+            result.First().EventIdentifier.Should().Be(events.First().EventIdentifier);
+            Assert.IsTrue(result.SequenceEqual(events));
+        }        
+    }
+}
