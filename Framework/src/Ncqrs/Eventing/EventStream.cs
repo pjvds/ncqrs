554a5a0d981f48d75440d4586b1ab598709b911e
pj@born2code.net <pj@born2code.net>
2010-06-12 14:38:49 +0200

    Added missing renames.

---
+++ Framework/src/Ncqrs/Eventing/EventStream.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+namespace Ncqrs.Eventing
+{
+    public class SourcedEventStream : IEnumerable<ISourcedEvent>
+    {
+        private Guid _eventSourceId;
+        private long _sequence;
+
+        public SourcedEventStream(Guid eventSourceId, long sequence)
+        {
+            _eventSourceId = eventSourceId;
+            _sequence = sequence;
+        }
+
+        [ContractInvariantMethod]
+        protected void ContractInvariants()
+        {
+            Contract.Invariant(_eventSourceId != Guid.Empty);
+            Contract.Invariant(_sequence >= 0);
+        }
+
+        public IEnumerator<ISourcedEvent> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
