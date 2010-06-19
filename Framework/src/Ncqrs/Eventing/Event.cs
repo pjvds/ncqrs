752f6519f4d38afbbf581950ba5a3ec74b3ae369
pjvds <pj@born2code.net>
2010-06-19 17:43:53 +0200

    Fixed set properties in Event class. They now have a setter with internal access, the PropertyBagConverter can set these properties now.

--- Framework/src/Ncqrs/Eventing/Event.cs
+++ Framework/src/Ncqrs/Eventing/Event.cs
@@ -11,14 +11,14 @@ namespace Ncqrs.Eventing
         /// <summary>
         /// Gets the unique identifier for this event.
         /// </summary>
-        public Guid EventIdentifier { get; private set; }
+        public Guid EventIdentifier { get; internal set; }
 
         /// <summary>
         /// Gets the time stamp for this event.
         /// </summary>
         /// <value>a <see cref="DateTime"/> UTC value that represents the point
         /// in time where this event occurred.</value>
-        public DateTime EventTimeStamp { get; private set; }
+        public DateTime EventTimeStamp { get; internal set; }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Event"/> class.
