b00fc36fbf1aadfacdb99eb71b4b709bce1b9688
Chris Chilvers <chilversc@googlemail.com>
2010-07-09 14:22:15 +0100

    create unit tests for serialization classes

---
+++ Framework/src/Ncqrs.Tests/Eventing/Storage/Serialization/EventConverterTests.cs
@@ -0,0 +1,161 @@
+using System;
+using FluentAssertions;
+using Ncqrs.Eventing.Storage;
+using Ncqrs.Eventing.Storage.Serialization;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace Ncqrs.Tests.Eventing.Storage.Serialization
+{
+    public class EventConverterTests
+    {
+        private IEventTypeResolver _typeResolver;
+        private IEventConverter _childConverter = new NullEventConverter();
+        private const string EventName = "bob";
+
+        [SetUp]
+        public void SetUp()
+        {
+            _typeResolver = MockRepository.GenerateStub<IEventTypeResolver>();
+            _typeResolver
+                .Stub(x => x.EventNameFor(typeof(AnEvent)))
+                .TentativeReturn()
+                .Return(EventName);
+
+            _childConverter = MockRepository.GenerateStub<IEventConverter>();
+        }
+
+        [Test]
+        public void Ctor()
+        {
+            Assert.DoesNotThrow(() => new EventConverter(_typeResolver));
+        }
+
+        [Test]
+        public void Ctor_typeResolver_null()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => new EventConverter(null));
+            ex.ParamName.Should().Be("typeResolver");
+        }
+
+        [Test]
+        public void Upgrade()
+        {
+            var event1 = CreateEvent("foo");
+            var event2 = CreateEvent("bar");
+            var fooConverter = MockRepository.GenerateStub<IEventConverter>();
+            var barConverter = MockRepository.GenerateStub<IEventConverter>();
+
+            var converter = new EventConverter(_typeResolver);
+            converter.AddConverter("foo", fooConverter);
+            converter.AddConverter("bar", barConverter);
+
+            converter.Upgrade(event1);
+            converter.Upgrade(event2);
+
+            fooConverter.AssertWasCalled(x => x.Upgrade(event1));
+            barConverter.AssertWasCalled(x => x.Upgrade(event2));
+        }
+
+        [Test]
+        public void Upgrade_unknown_event()
+        {
+            var converter = new EventConverter(_typeResolver);
+            Assert.DoesNotThrow(() => converter.Upgrade(CreateEvent()));
+        }
+
+        [Test]
+        public void Upgrade_theEvent_null()
+        {
+            var converter = new EventConverter(_typeResolver);
+            var ex = Assert.Throws<ArgumentNullException>(() => converter.Upgrade(null));
+            ex.ParamName.Should().Be("theEvent");
+        }
+
+        [Test]
+        public void AddConverter_ByType()
+        {
+            var anEvent = CreateEvent();
+            var converter = new EventConverter(_typeResolver);
+            converter.AddConverter(typeof(AnEvent), _childConverter);
+
+            converter.Upgrade(anEvent);
+
+            _childConverter.AssertWasCalled(x => x.Upgrade(anEvent));
+        }
+
+        [Test]
+        public void AddConverter_ByType_eventType_null()
+        {
+            var converter = new EventConverter(_typeResolver);
+            var ex = Assert.Throws<ArgumentNullException>(() => converter.AddConverter((Type) null, _childConverter));
+            ex.ParamName.Should().Be("eventType");
+        }
+
+        [Test]
+        public void AddConverter_ByType_converter_null()
+        {
+            var converter = new EventConverter(_typeResolver);
+            var ex = Assert.Throws<ArgumentNullException>(() => converter.AddConverter(typeof(AnEvent), null));
+            ex.ParamName.Should().Be("converter");
+        }
+
+        [Test]
+        public void AddConverter_ByName()
+        {
+            var anEvent = CreateEvent();
+            var converter = new EventConverter(_typeResolver);
+            converter.AddConverter(EventName, _childConverter);
+
+            converter.Upgrade(anEvent);
+
+            _childConverter.AssertWasCalled(x => x.Upgrade(anEvent));
+        }
+
+        [Test]
+        public void AddConverter_ByName_eventName_null()
+        {
+            var converter = new EventConverter(_typeResolver);
+            var ex = Assert.Throws<ArgumentNullException>(() => converter.AddConverter((string) null, _childConverter));
+            ex.ParamName.Should().Be("eventName");
+        }
+
+        [Test]
+        public void AddConverter_ByName_converter_null()
+        {
+            var converter = new EventConverter(_typeResolver);
+            var ex = Assert.Throws<ArgumentNullException>(() => converter.AddConverter(EventName, null));
+            ex.ParamName.Should().Be("converter");
+        }
+
+        [Test]
+        public void AddConverter_duplicate_name()
+        {
+            var converter = new EventConverter(_typeResolver);
+            converter.AddConverter(EventName, _childConverter);
+
+            var ex = Assert.Throws<ArgumentException>(() => converter.AddConverter(EventName, _childConverter));
+            ex.ParamName.Should().Be("eventName");
+            ex.Message.Should().StartWith("There is already a converter for event 'bob'.");
+        }
+
+
+        private StoredEvent<JObject> CreateEvent()
+        {
+            return CreateEvent(EventName);
+        }
+
+        private StoredEvent<JObject> CreateEvent(string name)
+        {
+            var id = new Guid("36C84164-B324-496A-9098-1A5D9945DD88");
+            var sourceId = new Guid("A710F584-7134-4BAB-9741-75A35F9C1E02");
+            var theDate = new DateTime(2000, 01, 01);
+
+            JObject obj = new JObject();
+            return new StoredEvent<JObject>(id, theDate, name, new Version(1, 0), sourceId, 1, obj);
+        }
+
+        private class AnEvent {}
+    }
+}
---
+++ Framework/src/Ncqrs.Tests/Eventing/Storage/Serialization/StringEventTranslatorTests.cs
@@ -0,0 +1,102 @@
+using System;
+using FluentAssertions;
+using Ncqrs.Eventing.Storage;
+using Ncqrs.Eventing.Storage.Serialization;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace Ncqrs.Tests.Eventing.Storage.Serialization
+{
+    public class StringEventTranslatorTests
+    {
+        private StringEventTranslator _translator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _translator = new StringEventTranslator();
+        }
+
+        [Test]
+        public void TranslateToCommon()
+        {
+            var obj = CreateEvent(new JObject(
+                new JProperty("Name", "Alice"),
+                new JProperty("Value", 10)));
+
+            var result = _translator.TranslateToRaw(obj);
+            result.EventIdentifier.Should().Be(obj.EventIdentifier);
+            result.EventSourceId.Should().Be(obj.EventSourceId);
+            result.EventSequence.Should().Be(obj.EventSequence);
+            result.EventTimeStamp.Should().Be(obj.EventTimeStamp);
+            result.EventVersion.Should().Be(obj.EventVersion);
+            result.Data.Should().Be("{\"Name\":\"Alice\",\"Value\":10}");
+
+        }
+
+        [Test]
+        public void TranslateToCommon_obj_null()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => _translator.TranslateToCommon(null));
+            ex.ParamName.Should().Be("obj");
+        }
+
+        [Test]
+        public void TranslateToRaw()
+        {
+            var obj = CreateEvent("{\"Name\":\"Alice\",\"Value\":10}");
+
+            var result = _translator.TranslateToCommon(obj);
+
+            result.EventIdentifier.Should().Be(obj.EventIdentifier);
+            result.EventSourceId.Should().Be(obj.EventSourceId);
+            result.EventSequence.Should().Be(obj.EventSequence);
+            result.EventTimeStamp.Should().Be(obj.EventTimeStamp);
+            result.EventVersion.Should().Be(obj.EventVersion);
+            result.Data.Should().NotBeNull();
+        }
+
+        [Test]
+        public void TranslateToRaw_obj_null()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => _translator.TranslateToRaw(null));
+            ex.ParamName.Should().Be("obj");
+        }
+
+
+        [Test]
+        public void Dates_use_iso_format()
+        {
+            var obj = CreateEvent(new JObject(new JProperty("Value", new DateTime(2000, 01, 02, 03, 04, 05, 006))));
+
+            var result = _translator.TranslateToRaw(obj);
+
+            result.Data.Should().Be("{\"Value\":\"2000-01-02T03:04:05.006\"}");
+        }
+
+        [Test]
+        public void Dates_respect_timezone()
+        {
+            var zone = new TimeSpan(0, 4, 0, 0);
+            var obj = CreateEvent(new JObject(new JProperty("Value", new DateTimeOffset(2000, 01, 02, 03, 04, 05, 006, zone))));
+
+            var result = _translator.TranslateToRaw(obj);
+
+            result.Data.Should().Be("{\"Value\":\"2000-01-02T03:04:05.006+04:00\"}");
+        }
+
+
+        private StoredEvent<T> CreateEvent<T>(T data)
+        {
+            return new StoredEvent<T>(
+                eventName: "bob",
+                eventVersion: new Version(3, 0) /* different on purpose */,
+                eventIdentifier: new Guid("402639D5-4106-4AE7-B210-45780C7A08F3"),
+                eventSourceId: new Guid("697BAB3C-E122-4C70-85B0-76750BC2D878"),
+                eventSequence: 1,
+                eventTimeStamp: new DateTime(2001, 4, 12, 12, 52, 21, 425, DateTimeKind.Utc),
+                data: data
+            );
+        }
+    }
+}
