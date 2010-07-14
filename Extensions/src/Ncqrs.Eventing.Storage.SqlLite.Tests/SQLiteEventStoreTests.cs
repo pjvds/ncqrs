9cb98ad375d630f7d53b72adbf8da4490fccaf53
Lee Henson <lee.m.henson@gmail.com>
2010-07-14 11:07:06 +0100

    Switch to tick-precision timestamps taken from event objects themselves

--- Extensions/src/Ncqrs.Eventing.Storage.SqlLite.Tests/SQLiteEventStoreTests.cs
+++ Extensions/src/Ncqrs.Eventing.Storage.SqlLite.Tests/SQLiteEventStoreTests.cs
@@ -80,6 +80,39 @@ namespace Ncqrs.Eventing.Storage.SQLite.Tests{
             result.Count().Should().Be(events.Length);
             result.First().EventIdentifier.Should().Be(events.First().EventIdentifier);
             Assert.IsTrue(result.SequenceEqual(events));
-        }        
+        }
+
+        [Test]
+        public void Retrieved_event_should_having_identical_timestamp_as_persisted() {
+            var id = Guid.NewGuid();
+            var utcNow = DateTime.UtcNow.Date.AddHours(9).AddTicks(-1);
+            
+            var events = new SourcedEvent[]
+            {
+                new CustomerCreatedEvent(Guid.NewGuid(), id, 0, utcNow, "Foo", 35)
+            };
+
+            var eventSource = MockRepository.GenerateMock<IEventSource>();
+            eventSource.Stub(e => e.EventSourceId).Return(id);
+            eventSource.Stub(e => e.InitialVersion).Return(0);
+            eventSource.Stub(e => e.Version).Return(events.Length);
+            eventSource.Stub(e => e.GetUncommittedEvents()).Return(events);
+            _store.Save(eventSource);
+
+            using (var conn = new SQLiteConnection(_connString))
+            {
+                conn.Open();
+
+                using (var cmd = new SQLiteCommand("SELECT [TimeStamp] FROM [Events]", conn))
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var timestamp = (long)reader["Timestamp"];
+                        timestamp.Should().Be(utcNow.Ticks);
+                    }
+                }
+            }
+        }
     }
 }
