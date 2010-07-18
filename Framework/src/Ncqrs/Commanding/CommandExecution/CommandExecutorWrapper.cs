d4eba5257e25353b86a02a03d9c6c48462506ac7
pjvds <pj@born2code.net>
2010-07-18 17:10:09 +0200

    Added CommandExecutorWrapper<TCommand> to own file and moved to CommandExecution root namespace.

---
+++ Framework/src/Ncqrs/Commanding/CommandExecution/CommandExecutorWrapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ncqrs.Commanding.CommandExecution
+{
+    public class CommandExecutorWrapper<TCommand> : ICommandExecutor<TCommand> where TCommand:ICommand
+    {
+        private readonly Action<TCommand> _action;
+
+        public CommandExecutorWrapper(Action<TCommand> action)
+        {
+            if(action == null) throw new ArgumentNullException("action");
+
+            _action = action;
+        }
+
+        public void Execute(TCommand command)
+        {
+            _action(command);
+        }
+    }
+}
