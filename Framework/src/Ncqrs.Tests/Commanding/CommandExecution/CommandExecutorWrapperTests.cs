0675005bfd031a37ab837b3d67cca8502548d5d8
pjvds <pj@born2code.net>
2010-07-19 20:58:32 +0200

    Added tests for CommandExecutorWrapper<TCommand> class.

---
+++ Framework/src/Ncqrs.Tests/Commanding/CommandExecution/CommandExecutorWrapperTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using Ncqrs.Commanding;
+using Ncqrs.Commanding.CommandExecution;
+using NUnit.Framework;
+
+namespace Ncqrs.Tests.Commanding.CommandExecution
+{
+    [TestFixture]
+    public class CommandExecutorWrapperTests
+    {
+        public class TheCommand : ICommand
+        {
+            public Guid CommandIdentifier
+            {
+                get; set;
+            }
+        }
+
+        [Test]
+        public void Constructing_it_with_a_null_action_should_throw()
+        {
+            Action<ICommand> nullAction = null; 
+            
+            Action act = ()=> new CommandExecutorWrapper<ICommand>(nullAction);
+
+            act.ShouldThrow<ArgumentNullException>().And.ParamName.Should().Be("action");
+        }
+
+        [Test]
+        public void Executig_it_should_redirect_call_to_initialized_action()
+        {
+            bool redirected = false;
+            Action<ICommand> action = (c) => redirected = true;
+            var cmd = new TheCommand();
+
+            var target = new CommandExecutorWrapper<ICommand>(action);
+            target.Execute(cmd);
+
+            redirected.Should().BeTrue();
+        }
+
+        [Test]
+        public void Executig_it_should_redirect_the_given_command_to_initialized_action()
+        {
+            var theCommand = new TheCommand();
+            Action<ICommand> action = (c) => c.Should().BeSameAs(theCommand);
+
+            var target = new CommandExecutorWrapper<ICommand>(action);
+            target.Execute(theCommand);
+        }
+    }
+}
