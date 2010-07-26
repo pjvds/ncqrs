381c200de53d2d1b5cebb0b9473c00281a507be9
pjvds <pj@born2code.net>
2010-07-27 00:06:20 +0200

    Added unit tests and documentation for InternalExtensions.

---
+++ Framework/src/Ncqrs.Tests/InternalExtensionsTests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using System.Collections;
+
+namespace Ncqrs.Tests
+{
+    [TestFixture]
+    public class InternalExtensionsTests
+    {
+        [Test]
+        public void IsEmpty_should_return_true_on_empty_array()
+        {
+            var target = new object[0];
+            var isEmpty = InternalExtensions.IsEmpty(target);
+
+            Assert.IsTrue(isEmpty);
+        }
+
+        [Test]
+        public void IsEmpty_should_return_true_on_empty_list()
+        {
+            var target = new List<object>(0);
+            var isEmpty = InternalExtensions.IsEmpty(target);
+
+            Assert.IsTrue(isEmpty);
+        }
+
+        [Test]
+        public void IsEmpty_should_return_false_on_non_empty_array()
+        {
+            var target = new object[1];
+            var isEmpty = InternalExtensions.IsEmpty(target);
+
+            Assert.IsFalse(isEmpty);
+        }
+
+        [Test]
+        public void IsEmpty_should_return_false_on_non_empty_list()
+        {
+            var target = new List<object>();
+            target.Add(new object());
+            var isEmpty = InternalExtensions.IsEmpty(target);
+
+            Assert.IsFalse(isEmpty);
+        }
+
+        [Test]
+        public void IsEmpty_should_throw_ArgumentNullException_on_null_reference()
+        {
+            List<object> target = null;
+            Action act = ()=>InternalExtensions.IsEmpty(target);
+
+            act.ShouldThrow<ArgumentNullException>().And.ParamName.Should().Be("source");
+        }
+
+        [Test]
+        public void IsNullOrEmpty_should_return_true_on_null()
+        {
+            var result = InternalExtensions.IsNullOrEmpty(null);
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public void IsNullOrEmpty_should_return_true_on_empty()
+        {
+            var result = InternalExtensions.IsNullOrEmpty(string.Empty);
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public void IsNullOrEmpty_should_return_false_on_string_with_text()
+        {
+            var result = InternalExtensions.IsNullOrEmpty("Hello world");
+            result.Should().BeFalse();
+        }
+
+        [Test]
+        public void Clone_should_return_other_instance()
+        {
+            var source = new List<object>();
+            var clone = InternalExtensions.Clone(source);
+
+            Assert.AreNotSame(source, clone);
+        }
+
+        [Test]
+        public void Clone_should_return_list_with_same_values()
+        {
+            var source = new List<int>{1,2,3,4,5};
+            var clone = InternalExtensions.Clone(source);
+
+            clone.Should().ContainInOrder(source);
+        }
+
+        [Test]
+        public void Clone_should_throw_ArgumentNullException_when_target_is_null()
+        {
+            List<string> nullTarget = null;
+            Action act = ()=> InternalExtensions.Clone(nullTarget);
+            act.ShouldThrow<ArgumentNullException>().And.ParamName.Should().Be("source");
+        }
+    }
+}
--- Framework/src/Ncqrs/InternalExtensions.cs
+++ Framework/src/Ncqrs/InternalExtensions.cs
@@ -1,36 +1,64 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Ncqrs
 {
+    /// <summary>
+    /// Provides general extensions that are used inside the framework code.
+    /// </summary>
     internal static class InternalExtensions
     {
-        public static bool IsEmpty(this IEnumerable target)
+        /// <summary>
+        /// Determine whether an IEnumerable source is empty or not.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the objects that can be enumerated.</typeparam>
+        /// <param name="source">The source to check. This may not be null.</param>
+        /// <exception cref="ArgumentNullException">Occurs when <paramref name="source"/> is <c>null</c>.</exception>
+        /// <returns><c>true</c> whenever the source did not contain any items to enumerate; otherwise, <c>false</c>.</returns>
+        public static bool IsEmpty<TSource>(this IEnumerable<TSource> source)
         {
-            return (target.Cast<object>().Count() == 0);
+            if (source == null) throw new ArgumentNullException("source");
+
+            return (source.Count() == 0);
         }
 
-        public static bool IsEmpty<TSource>(this IEnumerable<TSource> target)
+        /// <summary>
+        /// Determines whether a string is <c>null</c> or <see cref="String.Empty"/>
+        /// </summary>
+        /// <param name="source">The source to check.</param>
+        /// <returns>
+        /// 	<c>true</c> if <paramref name="source"/> is <c>null</c> or <see cref="String.Empty"/>; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsNullOrEmpty(this string source)
         {
-            return (target.Count() == 0);
+            return String.IsNullOrEmpty(source);
         }
 
-        public static bool IsNullOrEmpty(this string target)
+        /// <summary>
+        /// Clones a list. It returns a new list with the same content.
+        /// </summary>
+        /// <typeparam name="T">The type of the objects in the list.</typeparam>
+        /// <param name="source">The source to clone. This cannot be <c>null</c>.</param>
+        /// <exception cref="ArgumentNullException">Occurs when <paramref name="source"/> is <c>null</c>.</exception>
+        /// <returns>A new instance that contains the same elements in the same order as the source.</returns>
+        public static List<T> Clone<T>(this List<T> source)
         {
-            return String.IsNullOrEmpty(target);
+            if (source == null) throw new ArgumentNullException("source");
+
+            return new List<T>(source);
         }
 
-        public static List<T> Clone<T>(this List<T> target)
-        {
-            return new List<T>(target);
-        }
-
+        /// <summary>
+        /// Determines whether the specified type is nullable.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <returns>
+        /// 	<c>true</c> if the specified type is nullable; otherwise, <c>false</c>.
+        /// </returns>
         public static bool IsNullable(this Type type)
         {
             return !type.IsValueType || (type.IsGenericType && (type.GetGenericTypeDefinition() == typeof(Nullable<>)));
         }
-
     }
 }
