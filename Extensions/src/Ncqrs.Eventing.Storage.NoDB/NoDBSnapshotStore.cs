c8b5177d19a3502274fdb7f335e1e65cce708268
Chris Nicola <chnicola@gmail.com>
2010-08-02 15:26:17 -0700

    Ensured the snapshot store returns null if it has problems reading the data

--- Extensions/src/Ncqrs.Eventing.Storage.NoDB/NoDBSnapshotStore.cs
+++ Extensions/src/Ncqrs.Eventing.Storage.NoDB/NoDBSnapshotStore.cs
@@ -22,21 +22,40 @@ namespace Ncqrs.Eventing.Storage.NoDB
             if (!file.Exists && !file.Directory.Exists)
                 file.Directory.Create();
             var jo = JObject.FromObject(source);
-            source.EventSourceId.GetWriteLock("snapshot");
-            File.WriteAllText(file.FullName, source.GetType().AssemblyQualifiedName + "\n\r" + jo.ToString());
-            source.EventSourceId.ReleaseWriteLock("snapshot");
+            WriteSnapshotTest(source, file.FullName, jo.ToString());
         }
 
         public ISnapshot GetSnapshot(Guid eventSourceId)
         {
             FileInfo file = eventSourceId.GetSnapshotFileInfo(_path);
             if (!file.Exists) return null;
+            var reader = new StringReader(GetSnapshotText(eventSourceId, file.FullName));
+            var typeline = reader.ReadLine();
+            if (string.IsNullOrEmpty(typeline)) return null;
+            var type = Type.GetType(typeline.Trim());
+            try
+            {
+                return (ISnapshot) new JsonSerializer().Deserialize(reader, type);
+            }
+            catch(JsonSerializationException ex)
+            {
+                return null;
+            }
+        }
+
+        private static void WriteSnapshotTest(ISnapshot source, string path, string jsonData)
+        {
+            source.EventSourceId.GetWriteLock("snapshot");
+            File.WriteAllText(path, source.GetType().AssemblyQualifiedName + "\n\r" + jsonData);
+            source.EventSourceId.ReleaseWriteLock("snapshot");
+        }
+
+        private static string GetSnapshotText(Guid eventSourceId, string path)
+        {
             eventSourceId.GetReadLock("snapshot");
-            var snapshottext = File.ReadAllText(file.FullName);
+            var snapshottext = File.ReadAllText(path);
             eventSourceId.ReleaseReadLock("snapshot");
-            var reader = new StringReader(snapshottext);
-            var type = Type.GetType(reader.ReadLine().Trim());
-            return (ISnapshot) new JsonSerializer().Deserialize(reader, type);
+            return snapshottext;
         }
     }
 }
