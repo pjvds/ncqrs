e1a4f763d1f3b722ea86ae33d3e0d0666a5fde0a
Szymon Pobiega <szymon.pobiega@gmail.com>
2010-08-09 22:40:02 +0200

    temporary commit

--- Extensions/src/Ncqrs.EventBus/EventDemultiplexer.cs
+++ Extensions/src/Ncqrs.EventBus/EventDemultiplexer.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Ncqrs.EventBus
 {
+    public class EventDemultiplexerQueue
+    {
+        
+    }
+
     public class EventDemultiplexer
     {
-        private readonly IEventStore _eventStore;        
+        private readonly IEventStore _eventStore;
+        private readonly List<EventDemultiplexerQueue> _queues = new List<EventDemultiplexerQueue>();
 
         public EventDemultiplexer(IEventStore eventStore)
         {
