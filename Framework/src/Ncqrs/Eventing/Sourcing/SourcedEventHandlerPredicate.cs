7c2d964698c2ce85b996a46b8f5e39a47e9c47a0
pjvds <pj@born2code.net>
2010-08-28 15:43:11 +0200

    First version of entity support.

---
+++ Framework/src/Ncqrs/Eventing/Sourcing/SourcedEventHandlerPredicate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ncqrs.Eventing.Sourcing
+{
+    public class SourcedEventHandlerPredicate<TSourcedEvent> : TypeThresholdedActionBasedDomainEventHandler<TSourcedEvent>
+        where TSourcedEvent : SourcedEvent
+    {
+        private readonly Predicate<TSourcedEvent> _predicate;
+        private readonly ISourcedEventHandler _internalHandler;
+
+        public SourcedEventHandlerPredicate(Predicate<TSourcedEvent> predicate, ISourcedEventHandler internalHandler)
+            : base((e) => internalHandler.HandleEvent(e), false)
+        {
+            _predicate = predicate;
+            _internalHandler = internalHandler;
+        }
+    }
+}
