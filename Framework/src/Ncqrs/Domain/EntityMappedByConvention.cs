f72d1478768e7b5e88220fe95de8a041db5492d4
pjvds <pj@born2code.net>
2010-09-21 22:05:59 +0200

    Added generic EntityMappedByConvention<TAggregateRoot> class that has a strong typed root.
    An strong typed root was already supported by the Entity<TAggregateRoot> class. The non generic EntityMappedByConvention still exists, that class just inherits EntityMappedByConvention<AggregateRoot>.

--- Framework/src/Ncqrs/Domain/EntityMappedByConvention.cs
+++ Framework/src/Ncqrs/Domain/EntityMappedByConvention.cs
@@ -4,9 +4,17 @@ using Ncqrs.Eventing.Sourcing.Mapping;
 
 namespace Ncqrs.Domain
 {
-    public abstract class EntityMappedByConvention : Entity
+    public abstract class EntityMappedByConvention : EntityMappedByConvention<AggregateRoot>
     {
         protected EntityMappedByConvention(AggregateRoot parent, Guid entityId) : base(parent, entityId)
+        {
+        }
+    }
+
+    public abstract class EntityMappedByConvention<TAggregateRoot> : Entity<TAggregateRoot> where TAggregateRoot : AggregateRoot
+    {
+        protected EntityMappedByConvention(TAggregateRoot parent, Guid entityId)
+            : base(parent, entityId)
         {
             var mapping = new ConventionBasedEventHandlerMappingStrategy();
             var handlers = mapping.GetEventHandlers(this);
