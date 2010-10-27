12c7d3f886b7ca466c5bfe2b220194e8bfafab65
pjvds <pj@born2code.net>
2010-10-27 21:01:49 +0200

    Added tests for CommandExecutorBase<TCommand>.

---
+++ Framework/src/Ncqrs.Tests/Commanding/CommandExecutorBaseTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using FluentAssertions;
+using Ncqrs.Commanding.CommandExecution;
+using Ncqrs.Domain;
+using NUnit.Framework;
+using Ncqrs.Commanding;
+using Rhino.Mocks;
+
+namespace Ncqrs.Tests.Commanding
+{
+    [TestFixture]
+    public class CommandExecutorBaseTests
+    {
+        public class FooCommand : ICommand
+        {
+            public Guid CommandIdentifier { get; set; }
+
+            public FooCommand()
+            {
+                CommandIdentifier = Guid.NewGuid();
+            }
+        }
+
+        public class FooCommandExecutor : CommandExecutorBase<FooCommand>
+        {
+            public IUnitOfWorkContext LastGivenContext { get; private set;}
+
+            public FooCommand LastGivenCommand { get; private set; }
+
+            public FooCommandExecutor()
+            {
+            }
+
+            public FooCommandExecutor(IUnitOfWorkFactory unitOfWorkFactory) : base(unitOfWorkFactory)
+            {
+            }
+
+            protected override void ExecuteInContext(IUnitOfWorkContext context, FooCommand command)
+            {
+                LastGivenContext = context;
+                LastGivenCommand = command;
+            }
+        }
+
+        [Test]
+        public void Executing_one_with_a_custom_factory_should_give_context_created_with_that_factory()
+        {
+            var factory = MockRepository.GenerateMock<IUnitOfWorkFactory>();
+
+            var aCommand = new FooCommand();
+            var executor = new FooCommandExecutor(factory);
+            executor.Execute(aCommand);
+
+            factory.AssertWasCalled(f=>f.CreateUnitOfWork());
+        }
+
+        [Test]
+        public void Executing_should_call_ExecuteInContext_with_context_from_factory()
+        {
+            var context = MockRepository.GenerateMock<IUnitOfWorkContext>();
+            var factory = MockRepository.GenerateMock<IUnitOfWorkFactory>();
+            factory.Stub(f => f.CreateUnitOfWork()).Return(context);
+
+            var aCommand = new FooCommand();
+            var executor = new FooCommandExecutor(factory);
+            executor.Execute(aCommand);
+
+            executor.LastGivenContext.Should().Be(context);
+        }
+
+        [Test]
+        public void Executing_should_call_ExecuteInContext_with_given_command()
+        {
+            var theCommand = new FooCommand();
+            var factory = MockRepository.GenerateMock<IUnitOfWorkFactory>();
+
+            var executor = new FooCommandExecutor(factory);
+            executor.Execute(theCommand);
+
+            executor.LastGivenCommand.Should().Be(theCommand);
+        }
+    }
+}
