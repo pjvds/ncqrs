a570c4a36f15eeae2966c1da2782fc3e2d768258
James Bradt <james.bradt@gmail.com>
2010-10-28 17:56:05 -0400

    add missing ICommandMessage.cs

---
+++ Extensions/src/Ncqrs.NServiceBus/ICommandMessage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using nsb = NServiceBus;
+
+namespace Ncqrs.NServiceBus
+{
+    public interface ICommandMessage : nsb.IMessage
+    {
+        Ncqrs.Commanding.ICommand Payload { get; set; }
+    }
+}
