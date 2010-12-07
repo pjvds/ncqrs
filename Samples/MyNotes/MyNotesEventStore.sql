cefcfe1fe3b98843db6a7cc03ef8d5311df06d2a
pjvds <pj@born2code.net>
2010-12-07 23:01:02 +0100

    Updated Event Store database creation script in MyNotes example app to use Nvarchar column for event data.

--- Samples/MyNotes/MyNotesEventStore.sql
+++ Samples/MyNotes/MyNotesEventStore.sql
@@ -13,21 +13,30 @@ GO
 USE [MyNotesEventStore]
 GO
 
-CREATE TABLE [dbo].[EventSources](
-	[Id] [uniqueidentifier] PRIMARY KEY,
-	[Type] [nvarchar](255) NOT NULL,
-	[Version] [int] NOT NULL
-) ON [PRIMARY]
-
-
 CREATE TABLE [dbo].[Events]
 (
-	[Id] [uniqueidentifier] PRIMARY KEY,
+	[Id] [uniqueidentifier] NOT NULL,
 	[TimeStamp] [datetime] NOT NULL,
+
 	[Name] [varchar](max) NOT NULL,
 	[Version] [varchar](max) NOT NULL,
+
 	[EventSourceId] [uniqueidentifier] NOT NULL,
 	[Sequence] [bigint], 
-	[Data] [varbinary](max) NOT NULL
+
+	[Data] [nvarchar](max) NOT NULL
+) ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[EventSources]
+(
+	[Id] [uniqueidentifier] NOT NULL, [Type] [nvarchar](255) NOT NULL, [Version] [int] NOT NULL
+) ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[Snapshots]
+(
+	[EventSourceId] [uniqueidentifier] NOT NULL, [Version] [bigint], [TimeStamp] [datetime] NOT NULL, 
+	[Type] varchar(255) NOT NULL, [Data] [varbinary](max) NOT NULL
 ) ON [PRIMARY]
 GO
