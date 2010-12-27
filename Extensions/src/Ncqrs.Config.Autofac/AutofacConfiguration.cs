0a5c6dbb1af3ddc1c03350402eafde87bd15aace
unknown <mthird@.beelinewan.com>
2010-12-27 13:50:30 -0500

    added appropriate autodocs

--- Extensions/src/Ncqrs.Config.Autofac/AutofacConfiguration.cs
+++ Extensions/src/Ncqrs.Config.Autofac/AutofacConfiguration.cs
@@ -6,15 +6,33 @@ using Autofac;
 
 namespace Ncqrs.Config.Autofac
 {
+    /// <summary>
+    ///  A environment configuration based on Autofac.
+    /// </summary>
+    /// <code>
+    /// var builder = new ContainerBuilder();
+    /// // ... configure container
+    /// NcqrsEnvironment.Configure(new AutofacConfiguration(builder.Build()));
+    /// </code>
     public class AutofacConfiguration : IEnvironmentConfiguration
     {
         private readonly IContainer _container;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AutofacConfiguration"/> class.
+        /// </summary>
+        /// <param name="container">The Autofac container which will provide components to Ncqrs.</param>
         public AutofacConfiguration(IContainer container)
         {
             _container = container;
         }
 
+        /// <summary>
+        /// Tries to get the specified instance.
+        /// </summary>
+        /// <typeparam name="T">The type of the instance to get.</typeparam>
+        /// <param name="result">The result.</param>
+        /// <returns>An indication whether the instance could be get or not.</returns>
         public bool TryGet<T>(out T result) where T : class
         {
             if (!_container.TryResolve<T>(out result))
@@ -22,7 +40,7 @@ namespace Ncqrs.Config.Autofac
                 result = default(T);
             }
 
-            return result != null;
+            return result != default(T);
         }
     }
 }
