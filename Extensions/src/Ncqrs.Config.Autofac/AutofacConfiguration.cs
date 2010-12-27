8324eb30571d032dc3540fd5f34ead378efd99c5
unknown <mthird@.beelinewan.com>
2010-12-27 13:35:36 -0500

    added Autofac configuration support

---
+++ Extensions/src/Ncqrs.Config.Autofac/AutofacConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autofac;
+
+namespace Ncqrs.Config.Autofac
+{
+    public class AutofacConfiguration : IEnvironmentConfiguration
+    {
+        private readonly IContainer _container;
+
+        public AutofacConfiguration(IContainer container)
+        {
+            _container = container;
+        }
+
+        public bool TryGet<T>(out T result) where T : class
+        {
+            if (!_container.TryResolve<T>(out result))
+            {
+                result = default(T);
+            }
+
+            return result != null;
+        }
+    }
+}
