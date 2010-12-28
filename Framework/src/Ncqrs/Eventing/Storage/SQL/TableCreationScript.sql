80af855830f68ea25d44d6a88fe9f7c6f1a4efff
pjvds <pj@born2code.net>
2010-12-28 23:25:22 +0100

    Updated TableCreationScript of the Ms SQL based event store to support the new event bus that reads from it.

--- Framework/src/Ncqrs/Eventing/Storage/SQL/TableCreationScript.sql
+++ Framework/src/Ncqrs/Eventing/Storage/SQL/TableCreationScript.sql
@@ -1,18 +1,23 @@
-﻿CREATE TABLE [dbo].[Events]
-(
-	[SequentialId] [int] NOT NULL IDENTITY (1, 1)
+﻿CREATE TABLE [dbo].[Events](
+	[SequentialId] [int] IDENTITY(1,1) NOT NULL,
 	[Id] [uniqueidentifier] NOT NULL,
 	[TimeStamp] [datetime] NOT NULL,
-
 	[Name] [varchar](max) NOT NULL,
 	[Version] [varchar](max) NOT NULL,
-
 	[EventSourceId] [uniqueidentifier] NOT NULL,
-	[Sequence] [bigint], 
-
-	[Data] [nvarchar](max) NOT NULL
+	[Sequence] [bigint] NULL,
+	[Data] [nvarchar](max) NOT NULL,
+	CONSTRAINT [PK_Events] PRIMARY KEY CLUSTERED 
+	(
+		[SequentialId] ASC
+	)
+	WITH
+	(
+		PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF,
+		IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON,
+		ALLOW_PAGE_LOCKS  = ON
+	)
 ) ON [PRIMARY]
-GO
 
 CREATE TABLE [dbo].[EventSources]
 (
@@ -24,5 +29,21 @@ CREATE TABLE [dbo].[Snapshots]
 (
 	[EventSourceId] [uniqueidentifier] NOT NULL, [Version] [bigint], [TimeStamp] [datetime] NOT NULL, 
 	[Type] varchar(255) NOT NULL, [Data] [varbinary](max) NOT NULL
+) ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[PipelineState](
+	[BatchId] [int] IDENTITY(1,1) NOT NULL,
+	[LastProcessedEventId] [uniqueidentifier] NOT NULL,
+	CONSTRAINT [PK_MainPipelineState] PRIMARY KEY CLUSTERED 
+	(
+		[BatchId] ASC
+	)
+	WITH
+	(
+		PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF,
+		IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, 
+		ALLOW_PAGE_LOCKS  = ON
+	)
 ) ON [PRIMARY]
 GO
