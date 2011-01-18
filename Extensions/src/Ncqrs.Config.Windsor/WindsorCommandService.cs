d219120c296342550ab41792f65d322fb23e8502
chris.nicola <chris.nicola@visioncritical.com>
2011-01-18 12:43:14 -0800

    Missed dynamic cast in the WCS

--- Extensions/src/Ncqrs.Config.Windsor/WindsorCommandService.cs
+++ Extensions/src/Ncqrs.Config.Windsor/WindsorCommandService.cs
@@ -33,7 +33,7 @@ namespace Ncqrs.Config.Windsor
                 if (executor == null) throw new ExecutorForCommandNotFoundException(command.GetType());
                 context = new CommandContext(command, CommandExecutionState.Resolved);
                 interceptors.ForEach(i => i.OnBeforeExecution(context));
-                executor.Execute(command);
+                executor.Execute((dynamic) command);
                 context = new CommandContext(command, CommandExecutionState.Called);
             }
             catch (Exception ex)
