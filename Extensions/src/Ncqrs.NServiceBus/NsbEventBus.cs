085458fbea4bdcb03768fe9b2b117d5886980efb
Szymon Pobiega <szymon.pobiega@gmail.com>
2011-01-19 18:26:02 -0500

    Fixed NSB integration.

--- Extensions/src/Ncqrs.NServiceBus/NsbEventBus.cs
+++ Extensions/src/Ncqrs.NServiceBus/NsbEventBus.cs
@@ -12,24 +12,24 @@ namespace Ncqrs.NServiceBus
     /// <summary>
     /// A <see cref="IEventBus"/> implementation using NServiceBus. Forwards all published
     /// events via NServiceBus. Does NOT support registering local event handlers using
-    /// <see cref="RegisterHandler{TEvent}"/>. All events passed to <see cref="Publish(System.Collections.Generic.IEnumerable{Ncqrs.Eventing.IEvent})"/>
+    /// <see cref="RegisterHandler{TEvent}"/>. All events passed to <see cref="Publish(Ncqrs.Eventing.ServiceModel.Bus.IPublishedEvent{Ncqrs.Eventing.ServiceModel.Bus.IPublishableEvent})"/>
     /// method are send using of NServiceBus transport level message.
     /// </summary>
     public class NsbEventBus : IEventBus
     {
-        public void Publish(IEvent eventMessage)
+        public void Publish(IPublishableEvent eventMessage)
         {
             Bus.Publish(CreateEventMessage(eventMessage));
         }
 
-        public void Publish(IEnumerable<IEvent> eventMessages)
+        public void Publish(IEnumerable<IPublishableEvent> eventMessages)
         {
             Bus.Publish(eventMessages.Select(CreateEventMessage).ToArray());
         }
 
-        public void RegisterHandler<TEvent>(IEventHandler<TEvent> handler) where TEvent : IEvent
+        public void RegisterHandler<TEvent>(IEventHandler<TEvent> handler)
         {
-            throw new NotSupportedException("Registering local event handlers with NsbEventBus is not supported yet.");
+            throw new NotSupportedException("Registering local event handlers with NsbEventBus is not supported. Use CompositeEventBus instead.");
         }
 
         private static IBus Bus
