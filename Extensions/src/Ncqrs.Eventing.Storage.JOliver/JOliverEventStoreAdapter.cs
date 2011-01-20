34b5382442de50222324a78fe2f3f96bc62fcdf1
Szymon Pobiega <szymon.pobiega@gmail.com>
2011-01-19 23:40:53 -0500

    Added adapter for JO event store

---
+++ Extensions/src/Ncqrs.Eventing.Storage.JOliver/JOliverEventStoreAdapter.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EventStore;
+using EventStore.Dispatcher;
+using EventStore.Persistence;
+
+namespace Ncqrs.Eventing.Storage.JOliver
+{
+    public class JOliverEventStoreAdapter : IEventStore
+    {
+        private readonly IStoreEvents _wrappedEventStore;
+
+        public JOliverEventStoreAdapter(IStoreEvents delegatedEventStore)
+        {
+            _wrappedEventStore = delegatedEventStore;
+        }
+
+        public JOliverEventStoreAdapter(IPersistStreams streamPersister)
+        {
+            _wrappedEventStore = new OptimisticEventStore(streamPersister, new NullDispatcher());
+        }
+
+        public CommittedEventStream ReadUntil(Guid id, long? maxVersion)
+        {
+            var maxRevision = maxVersion ?? int.MaxValue;
+            var originalStream = _wrappedEventStore.ReadUntil(id, (int)maxRevision);
+            return new CommittedEventStream(originalStream.Events.Cast<StoredEvent>().Select(x => Convert(x, id)));
+        }
+
+        private static CommittedEvent Convert(StoredEvent x, Guid id)
+        {
+            return new CommittedEvent(x.CommitId, x.EventId, id, x.Sequence, x.TimeStamp, x.Body,
+                                      new Version(x.MajorVersion, x.MinorVersion));
+        }
+
+        public CommittedEventStream ReadFrom(Guid id, long minVersion)
+        {
+            var originalStream = _wrappedEventStore.ReadFrom(id, (int)minVersion);
+            return new CommittedEventStream(originalStream.Events.Cast<StoredEvent>().Select(x => Convert(x, id)));
+        }
+
+        public void Store(UncommittedEventStream eventStream)
+        {
+            if (!eventStream.HasSingleSource)
+            {
+                throw new NotSupportedException("This event store don't support events streams with multiple sources.");
+            }
+            var sourceInformation = eventStream.Sources.Single();
+            var commitAttempt = new CommitAttempt
+                                    {
+                                        CommitId = eventStream.CommitId,
+                                        PreviousCommitSequence = (int) sourceInformation.InitialVersion,
+                                        StreamId = eventStream.SourceId,
+                                        StreamRevision = (int)sourceInformation.CurrentVersion,                                        
+                                    };
+            commitAttempt.Events.AddRange(eventStream.Select(x => new EventMessage
+                                                                      {
+                                                                          Body = new StoredEvent
+                                                                                     {
+                                                                                         Body = x.Payload,
+                                                                                         CommitId = eventStream.CommitId,
+                                                                                         EventId = x.EventIdentifier,
+                                                                                         MajorVersion = x.EventVersion.Major,
+                                                                                         MinorVersion = x.EventVersion.Minor,
+                                                                                         Sequence = x.EventSequence,
+                                                                                         TimeStamp = x.EventTimeStamp
+                                                                                     }
+                                                                      }));
+            _wrappedEventStore.Write(commitAttempt);
+        }
+
+        private class NullDispatcher : IDispatchCommits
+        {
+            public void Dispose()
+            {                
+            }
+
+            public void Dispatch(Commit commit)
+            {             
+            }
+        }
+    }
+}
---
+++ Extensions/src/Ncqrs.Eventing.Storage.JOliver/StoredEvent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Ncqrs.Eventing.Storage.JOliver
+{
+    [Serializable]
+    [DataContract]
+    public class StoredEvent
+    {
+        [DataMember]
+        public DateTime TimeStamp { get; set; }
+        [DataMember]
+        public long Sequence { get; set; }
+        [DataMember]
+        public object Body { get; set; }
+        [DataMember]
+        public Guid CommitId { get; set; }
+        [DataMember]
+        public Guid EventId { get; set; }
+        [DataMember]
+        public int MajorVersion { get; set; }
+        [DataMember]
+        public int MinorVersion { get; set; }
+    }
+}
