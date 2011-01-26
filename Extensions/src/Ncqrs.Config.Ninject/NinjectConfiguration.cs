595680d88a19354e86a7b64a846f56a8f020c0b0
Jason Dentler <jasondentler@gmail.com>
2011-01-25 20:15:52 -0600

    Added Ninject IoC configuration

---
+++ Extensions/src/Ncqrs.Config.Ninject/NinjectConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Ninject;
+
+namespace Ncqrs.Config.Ninject
+{
+
+    /// <summary>
+    /// A environment configuration based on Ninject. All requested instances
+    /// will be get from the Ninject <see cref="IKernel"/>.
+    /// </summary>
+    /// <code>
+    /// var kernel = new Ninject.StandardKernel(new NcqrsModule());
+    /// var config = new NinjectConfiguration(kernel);
+    /// NcqrsEnvironment.Configure(config);
+    /// </code>
+    public class NinjectConfiguration : IEnvironmentConfiguration
+    {
+
+        private readonly IKernel _kernel;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NinjectConfiguration" /> class.
+        /// </summary>
+        /// <param name="kernel">The Ninject kernel which will provide components to Ncqrs</param>
+        public NinjectConfiguration(IKernel kernel)
+        {
+            _kernel = kernel;
+        }
+
+        public bool TryGet<T>(out T result) where T : class
+        {
+            result = _kernel.TryGet<T>();
+            return result != null;
+        }
+    }
+
+}
---
+++ Ncqrs.Config.Ninject.Tests/NinjectConfigurationTests.cs
@@ -0,0 +1,42 @@
+﻿using Ninject;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Ncqrs.Config.Ninject.Tests
+{
+    [TestFixture]
+    public class NinjectConfigurationTests
+    {
+        [Test]
+        public void When_component_is_registered_it_should_be_retrievable()
+        {
+            var kernel = new StandardKernel();
+            kernel.Bind<IReplicant>().To<Nexus6>();
+
+            var configuration = new NinjectConfiguration(kernel);
+
+            IReplicant component;
+            var success = configuration.TryGet(out component);
+
+            success.Should().BeTrue();
+            component.Should().NotBeNull();
+            component.Should().BeOfType<IReplicant>();
+        }
+
+        [Test]
+        public void When_component_is_not_registered_it_should_not_be_retrievable()
+        {
+            var kernel = new StandardKernel();
+            var configuration = new NinjectConfiguration(kernel);
+
+            IReplicant component;
+            var success = configuration.TryGet(out component);
+
+            success.Should().BeFalse();
+            component.Should().BeNull();
+        }
+    }
+
+    public interface IReplicant { }
+    public class Nexus6 : IReplicant { }
+}
