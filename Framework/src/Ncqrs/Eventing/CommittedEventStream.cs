da3bfc5fafcd2e9574063c568acf0f0fa840bcf1
pjvds <pj@born2code.net>
2011-02-20 15:50:14 +0100

    Added event stream validation to CommittedEventStream.
    fixes #25

--- Framework/src/Ncqrs/Eventing/CommittedEventStream.cs
+++ Framework/src/Ncqrs/Eventing/CommittedEventStream.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 
 namespace Ncqrs.Eventing
 {
@@ -59,9 +60,12 @@ namespace Ncqrs.Eventing
 
         public CommittedEventStream(Guid sourceId, IEnumerable<CommittedEvent> events)
         {
-            // TODO: ValidateEventInformation(events);
             _sourceId = sourceId;
-            _events = events.ToList();
+
+            if(events != null) _events = new List<CommittedEvent>(events);
+
+            ValidateEventInformation(_events);
+            
             if (_events.Count > 0)
             {
                 var first = _events.First();
@@ -74,8 +78,53 @@ namespace Ncqrs.Eventing
             }
         }
 
-        //private static void ValidateEventInformation(IEnumerable<CommittedEvent> events)
-        //{
+        private void ValidateEventInformation(IEnumerable<CommittedEvent> events)
+        {
+            // An empty event stream is allowed.
+            if (events == null || events.IsEmpty()) return;
+
+            var firstEvent = events.First();
+            var startSequence = firstEvent.EventSequence;
+
+            var expectedSourceId = _sourceId;
+            var expectedSequence = startSequence;
+
+            ValidateSingleEvent(firstEvent, 0, expectedSourceId, expectedSequence);
+
+            for(int position = 1; position < _events.Count; position++)
+            {
+                var evnt = _events[position];
+                expectedSourceId = _sourceId;
+                expectedSequence = startSequence + position;
+
+                ValidateSingleEvent(evnt, position, expectedSourceId, expectedSequence);
+            }
+        }
+
+        private void ValidateSingleEvent(CommittedEvent evnt, long position, Guid expectedSourceId, long expectedSequence)
+        {
+            if (evnt == null)
+                throw new ArgumentNullException("events", "The events stream contains a null reference at position " + position + ".");
+
+            if (evnt.EventSourceId != expectedSourceId)
+            {
+                var msg = string.Format("The events stream contains an event that is related to another event " +
+                                        "source at position {0}. Expected event source id {1}, but actual was {2}",
+                                        position, _sourceId, evnt.EventSourceId);
+                throw new ArgumentException("events", msg);
+            }
+
+            if (evnt.EventSequence != expectedSequence)
+            {
+                var msg =
+                    string.Format("The events stream contains an committed event with an illigal sequence at " +
+                                  "position {0}. The expected sequence is {1}, but actual was {2}.",
+                                  position, expectedSequence, evnt.EventSequence);
+
+                throw new ArgumentException("events", msg);
+            }
+        }
+
         //    if(events.IsEmpty())
         //        return;
 
@@ -96,5 +145,28 @@ namespace Ncqrs.Eventing
         //            throw new ArgumentOutOfRangeException("events", msg);
         //        }
         //}
+    }
+
+    [Serializable]
+    public class InvalidCommittedEventException : Exception
+    {
+        public InvalidCommittedEventException()
+        {
+        }
+
+        public InvalidCommittedEventException(string message) : base(message)
+        {
+        }
+
+        public InvalidCommittedEventException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected InvalidCommittedEventException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+            
+        }
     }
 }
