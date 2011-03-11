d8cdaac7488962aebdb50a8fd95b88c7e4971a41
Jason Dentler <jasondentler@gmail.com>
2011-03-11 11:04:50 -0600

    New event serialization fixtures and examples. fixes #53

---
+++ Samples/MyNotes/src/Tests/when_serializing_NoteTextChanged.cs
@@ -0,0 +1,17 @@
+﻿using Events;
+using Ncqrs.Spec;
+
+namespace Tests
+{
+    public class when_serializing_NoteTextChanged
+        : JsonEventSerializationFixture<NoteTextChanged>
+    {
+        protected override NoteTextChanged GivenEvent()
+        {
+            return new NoteTextChanged()
+                       {
+                           NewText = "New text goes here."
+                       };
+        }
+    }
+}
