8d22be32005c91dd95750c836dbf48d4fc582907
Eric Scherrer <escherrer@gmail.com>
2011-03-14 20:01:43 -0400

    Added MS Unity IoC extension and test

---
+++ Extensions/src/Ncqrs.Config.Unity.Tests/UnityConfigurationTest.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using Microsoft.Practices.Unity;
+using NUnit.Framework;
+
+namespace Ncqrs.Config.Unity.Tests
+{
+    [TestFixture]
+    public class UnityConfigurationTest
+    {
+        [Test]
+        public void When_component_is_registered_it_should_be_retrievable()
+        {
+            var container = new UnityContainer();
+            var nex = new Nexus6();
+            container.RegisterInstance<IReplicant>(nex);
+
+            var configuration = new UnityConfiguration(container);
+
+            IReplicant component;
+            var success = configuration.TryGet(out component);
+
+            success.Should().BeTrue();
+            component.Should().NotBeNull();
+            component.Should().BeAssignableTo<IReplicant>();
+            component.Should().BeSameAs(nex);
+        }
+
+        [Test]
+        public void When_component_is_not_registered_it_should_not_be_retrievable()
+        {
+            var container = new UnityContainer();
+            var configuration = new UnityConfiguration(container);
+
+            IReplicant component;
+            var success = configuration.TryGet(out component);
+
+            success.Should().BeFalse();
+            component.Should().BeNull();
+        }
+    }
+
+    public interface IReplicant { }
+    public class Nexus6 : IReplicant { }
+}
---
+++ Extensions/src/Ncqrs.Config.Unity/UnityConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Practices.Unity;
+
+namespace Ncqrs.Config.Unity
+{
+    public class UnityConfiguration : IEnvironmentConfiguration
+    {
+        private readonly IUnityContainer _container;
+
+        public UnityConfiguration(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        public bool TryGet<T>(out T result) where T : class
+        {
+            result = _container.IsRegistered<T>() ? _container.Resolve<T>() : default(T);
+
+            return result != null;
+        }
+    }
+}
