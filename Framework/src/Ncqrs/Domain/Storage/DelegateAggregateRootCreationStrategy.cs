f3f2794809116f52f2b74392bd5363aafe93564a
Jason Dentler <jasondentler@gmail.com>
2011-04-01 09:31:17 -0500

    New aggregate root creation strategies. fixes #76

---
+++ Framework/src/Ncqrs/Domain/Storage/DelegateAggregateRootCreationStrategy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ncqrs.Domain.Storage
+{
+    public class DelegateAggregateRootCreationStrategy
+        : AggregateRootCreationStrategy
+    {
+        private readonly Func<Type, AggregateRoot> _factoryMethod;
+
+        public DelegateAggregateRootCreationStrategy(Func<Type, AggregateRoot> factoryMethod)
+        {
+            _factoryMethod = factoryMethod;
+        }
+
+        protected override AggregateRoot CreateAggregateRootFromType(Type aggregateRootType)
+        {
+            return _factoryMethod(aggregateRootType);
+        }
+
+    }
+}
