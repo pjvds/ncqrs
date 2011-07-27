2e2c10589ca78079719b58adf5e3f33036a0bcf2
ashic <ashic@live.com>
2011-07-27 14:49:53 +0100

    added method to register CommandExecutorBase<> implementations with the command service

--- Framework/src/Ncqrs/Commanding/CommandExecution/Mapping/Attributes/CommandServiceExtensions.cs
+++ Framework/src/Ncqrs/Commanding/CommandExecution/Mapping/Attributes/CommandServiceExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Reflection;
 using Ncqrs.Commanding.ServiceModel;
 
@@ -12,10 +13,32 @@ namespace Ncqrs.Commanding.CommandExecution.Mapping.Attributes
         {
             var mapper = new AttributeBasedCommandMapper();
 
-            foreach(var mappedCommand in asm.GetTypes().Where(mapper.CanMapCommand))
+            foreach (var mappedCommand in asm.GetTypes().Where(mapper.CanMapCommand))
             {
                 var executor = new UoWMappedCommandExecutor(mapper);
                 target.RegisterExecutor(mappedCommand, executor);
+            }
+        }
+    }
+
+    public static class RegisterAllExplicitCommandExecutorsInAssemblyExtension
+    {
+        public static void RegisterAllExplicitCommandExecutorsInAssembly(this CommandService target, Assembly asm)
+        {
+            var targetType = typeof(ICommandExecutor<>);
+            var types = asm.GetExportedTypes();
+            var commandExecutorTypes = types
+                .Where(x =>
+                    x.IsInterface == false &&
+                    x.IsAbstract == false &&
+                    x.GetInterface(targetType.FullName) != null);
+
+            foreach (var executorType in commandExecutorTypes)
+            {
+                dynamic executor = Activator.CreateInstance(executorType, true);
+                var commandType = executorType.GetInterface(targetType.FullName).GetGenericArguments().First();
+
+                target.RegisterExecutor(commandType, executor);
             }
         }
     }
