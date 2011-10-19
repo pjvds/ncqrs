64f6d0278f25e13d22b2b1672da3620d92755160
Dennis Traub <dennis.traub@gmail.com>
2011-10-19 18:13:41 +0200

    Added Ncqrs.Eventing.EventServiceMappedByConvention as a to provide out-of-context event publishing capabilities

---
+++ Framework/src/Ncqrs/Eventing/EventServiceMappedByConvention.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Ncqrs.Domain;
+using Ncqrs.Eventing.Sourcing;
+
+namespace Ncqrs.Eventing
+{
+	public abstract class EventServiceMappedByConvention : AggregateRootMappedByConvention
+	{
+		public void Raise(ISourcedEvent e)
+		{
+			var factory = NcqrsEnvironment.Get<IUnitOfWorkFactory>();
+			var context = factory.CreateUnitOfWork(e.EventIdentifier);
+
+			ApplyEvent(e);
+
+			context.Accept();
+		}
+	}
+}
