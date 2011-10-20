035e3faf11ecf4ba2088ecb3aad2bc6fb9c9ba88
Matt Sullivan <matt.sullivan@fei.com>
2011-10-20 16:45:41 +1000

    Add AutofacAggregateRootCreationStrategy
    Use Autofac to create AggregateRoots for the domain repository, allows the domain repository to re-construct AggregateRoots to their current state, but also have dependencies injected.

---
+++ Extensions/src/Ncqrs.Config.Autofac/AutofacAggregateRootCreationStrategy.cs
@@ -0,0 +1,29 @@
+﻿namespace Ncqrs.Config.Autofac
+{
+    using System;
+    using Ncqrs.Domain;
+    using Ncqrs.Domain.Storage;
+
+    using global::Autofac;
+
+    /// <summary>
+    /// Use Autofac to create AggregateRoots for the domain repository. (This allows the domain
+    /// repository to re-construct AggregateRoots to their current state, but also have
+    /// dependencies injected.
+    /// </summary>
+    public class AutofacAggregateRootCreationStrategy : AggregateRootCreationStrategy
+    {
+        private readonly ILifetimeScope _containerScope;
+
+        public AutofacAggregateRootCreationStrategy(ILifetimeScope containerScope)
+        {
+            _containerScope = containerScope;
+        }
+
+        protected override AggregateRoot CreateAggregateRootFromType(Type aggregateRootType)
+        {
+            return (AggregateRoot)_containerScope.Resolve(aggregateRootType);
+        }
+
+    }
+}
