653b8a17477ba512a5fafd7ec155f8111f632d32
Tom <tcboring@msn.com>
2013-05-09 08:08:51 -0700

    Added unique indexes and foreign keys to Ncqrs.Eventing.Storage.SQL.TableCreationScript.sql

--- Framework/src/Ncqrs/Eventing/Storage/SQL/TableCreationScript.sql
+++ Framework/src/Ncqrs/Eventing/Storage/SQL/TableCreationScript.sql
@@ -1,39 +1,4 @@
-﻿IF EXISTS(SELECT * FROM sysobjects WHERE xtype = 'U' AND name = 'Events')
-	BEGIN
-		PRINT 'The Events table already exists.'
-	END
-ELSE
-	BEGIN
-		CREATE TABLE [Events]
-		(
-			[SequentialId]			[int] IDENTITY(1,1)		NOT NULL,
-			[Id]					[uniqueidentifier]		NOT NULL,
-			[TimeStamp]				[datetime]				NOT NULL,
-			[Name]					[varchar](max)			NOT NULL,
-			[Version]				[varchar](max)			NOT NULL,
-			[EventSourceId]			[uniqueidentifier]		NOT NULL,
-			[Sequence]				[bigint]				NULL,
-			[Data]					[nvarchar](max)			NOT NULL,
-			CONSTRAINT [PK_Events] PRIMARY KEY CLUSTERED
-			(
-				[SequentialId] ASC
-			)
-			WITH
-			(
-				PAD_INDEX				= OFF,
-				STATISTICS_NORECOMPUTE	= OFF,
-				IGNORE_DUP_KEY			= OFF,
-				ALLOW_ROW_LOCKS			= ON,
-				ALLOW_PAGE_LOCKS		= ON
-			)
-		) ON [PRIMARY]
-
-		CREATE NONCLUSTERED INDEX IX_EventSourceId ON [Events] (EventSourceId)
-
-		PRINT 'The Events table was created.'
-	END;
-
-IF EXISTS(SELECT * FROM sysobjects WHERE xtype = 'U' AND name = 'EventSources')
+﻿IF EXISTS(SELECT * FROM sysobjects WHERE xtype = 'U' AND name = 'EventSources')
 	BEGIN
 		PRINT 'The EventSources table already exists.'
 	END
@@ -65,6 +30,51 @@ ELSE
 		PRINT 'The EventSuorces table was created.'
 	END
 
+IF EXISTS(SELECT * FROM sysobjects WHERE xtype = 'U' AND name = 'Events')
+	BEGIN
+		PRINT 'The Events table already exists.'
+	END
+ELSE
+	BEGIN
+		CREATE TABLE [Events]
+		(
+			[SequentialId]			[int] IDENTITY(1,1)		NOT NULL,
+			[Id]					[uniqueidentifier]		NOT NULL,
+			[TimeStamp]				[datetime]				NOT NULL,
+			[Name]					[varchar](max)			NOT NULL,
+			[Version]				[varchar](max)			NOT NULL,
+			[EventSourceId]			[uniqueidentifier]		NOT NULL,
+			[Sequence]				[bigint]				NULL,
+			[Data]					[nvarchar](max)			NOT NULL,
+			CONSTRAINT [PK_Events] PRIMARY KEY CLUSTERED
+			(
+				[SequentialId] ASC
+			)
+			WITH
+			(
+				PAD_INDEX				= OFF,
+				STATISTICS_NORECOMPUTE	= OFF,
+				IGNORE_DUP_KEY			= OFF,
+				ALLOW_ROW_LOCKS			= ON,
+				ALLOW_PAGE_LOCKS		= ON
+			)
+		) ON [PRIMARY]
+
+		CREATE NONCLUSTERED INDEX IX_EventSourceId ON [Events] (EventSourceId)
+
+		ALTER TABLE [Events]
+			ADD CONSTRAINT UQ_Events_Id UNIQUE ([Id])
+
+		ALTER TABLE [Events] WITH CHECK
+			ADD CONSTRAINT [FK_Events_EventSources] FOREIGN KEY([EventSourceId])
+			REFERENCES [EventSources] ([Id])
+
+		ALTER TABLE [Events]
+			CHECK CONSTRAINT [FK_Events_EventSources]
+
+		PRINT 'The Events table was created.'
+	END;
+
 IF EXISTS(SELECT * FROM sysobjects WHERE xtype = 'U' AND name = 'Snapshots')
 	BEGIN
 		PRINT 'The Snapshots table already exists.'
@@ -79,6 +89,13 @@ ELSE
 			[Type]					varchar(255)			NOT NULL,
 			[Data]					[varbinary](max)		NOT NULL
 		) ON [PRIMARY]
+
+		ALTER TABLE [Snapshots] WITH CHECK
+			ADD CONSTRAINT [FK_Snapshots_EventSources] FOREIGN KEY([EventSourceId])
+			REFERENCES [EventSources] ([Id])
+
+		ALTER TABLE [Snapshots]
+			CHECK CONSTRAINT [FK_Snapshots_EventSources]
 
 		PRINT 'The Snapshots table was created.'
 	END
@@ -107,6 +124,13 @@ ELSE
 				ALLOW_PAGE_LOCKS		= ON
 			)
 		) ON [PRIMARY]
+
+		ALTER TABLE [PipelineState] WITH CHECK
+			ADD CONSTRAINT [FK_PipelineState_Events] FOREIGN KEY([LastProcessedEventId])
+			REFERENCES [EventStore].[Events] ([Id])
+
+		ALTER TABLE [PipelineState]
+			CHECK CONSTRAINT [FK_PipelineState_Events]
 
 		PRINT 'The PipelineState table was created.'
 	END
